415704183767b49660a799f1b646337d0cca0642
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-09-18 01:20:36 -0500

    ready

--- Client/components/auth/withAdminAuth.js
+++
@@ -1,9 +0,0 @@
-// frontend/components/auth/withAdminAuth.js
-import withAuth from './withAuth';
-
-// Simple wrapper around withAuth specifically for admin role
-const withAdminAuth = (WrappedComponent) => {
-  return withAuth(WrappedComponent, { requiredRole: 'admin' });
-};
-
-export default withAdminAuth;
