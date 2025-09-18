579213661ddd168af0bb6e3ba6e8e13320b511ee
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-09-18 01:24:19 -0500

    ready

---
+++ SoftwareDT/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-black text-white font-custom-bold flex gap-4 md:gap-12 text-xs py-8 justify-center">
+        <p className="font-custom">SPACEX © 2024</p>
+        <a href="/">PRIVACY POLICY</a>
+        <a href="/">SUPPLIERS</a>
+    </div>
+  )
+}
+
+export default Footer
