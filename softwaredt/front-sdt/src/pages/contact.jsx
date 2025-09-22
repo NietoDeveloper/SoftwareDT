7868aeb48108bd4074b44e5ce20f0a56e6d3bef4
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-09-22 02:52:48 -0500

    Update contact.jsx

--- softwaredt/front-sdt/src/pages/contact.jsx
+++ softwaredt/front-sdt/src/pages/contact.jsx
@@ -1,2 +1,7 @@
 import react from "react";
-import 
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useState, useEffect } from "react";
+import "../index.css";
+
+
