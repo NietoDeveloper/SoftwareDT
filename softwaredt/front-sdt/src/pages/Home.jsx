4e3d4b1b57ebb53b1b39a5dc0820c2e45c9768ed
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-09-25 01:39:24 -0500

    footer
    active

--- softwaredt/front-sdt/src/pages/Home.jsx
+++ softwaredt/front-sdt/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Questions from "../components/Faq/Questions";
 import Guide from "../components/Explainer/Guide";
 import BogotaAir1 from "../assets/images/BogotaAir1.mp4";
 import BogotaAir2 from "../assets/images/MonserrateDron1.mp4";
+import Footer from "../components/"
 
 const Home = () => {
   return (
@@ -49,6 +50,8 @@ const Home = () => {
           src={BogotaAir2}
         />
       </section>
+
+      <Footer />
 
     </>
   );
