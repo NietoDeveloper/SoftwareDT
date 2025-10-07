b707090a3fc78ab18c69d152f9a6eda3d0f77fe0
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-06 23:42:06 -0500

    Update notFound.jsx

--- softwaredt/front-sdt/src/pages/notFound.jsx
+++ softwaredt/front-sdt/src/pages/notFound.jsx
@@ -4,7 +4,7 @@ const NotFoundPage = () => {
   return (
     <div className="container">
         <div>
-           <h1 className="text-center text-red-600 text-3xl">404 - Page Not Found</h1> 
+           <h1 className="text-center text-red-600 text-3xl">404 - Page Not Found </h1> 
         </div>
         
     </div>
