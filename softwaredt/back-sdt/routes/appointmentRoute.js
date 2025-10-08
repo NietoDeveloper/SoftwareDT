bfcabdcd31cefb0b12b59c7ff96146035f4cdd89
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-08 18:59:08 -0500

    Update appointmentRoute.js

--- softwaredt/back-sdt/routes/appointmentRoute.js
+++ softwaredt/back-sdt/routes/appointmentRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { appointmentBooking } = require('../controllers/appointmentController');
 
 // Route to handle appointment booking
+
 router.route('/:id').
       post(appointmentBooking);
 
