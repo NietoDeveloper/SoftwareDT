3787ac7647be0daf4059e25c84a7642c0c732491
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-08 18:57:51 -0500

    Update appointmentController.js

--- softwaredt/back-sdt/controllers/appointmentController.js
+++ softwaredt/back-sdt/controllers/appointmentController.js
@@ -40,7 +40,7 @@ const appointmentBooking = asyncHandler(async (req, res) => {
         }
 
     } catch (error) {
-        return res.status(500).json({ message: "Error de servidor encontrado mientra agendaba la .", error: error.message });
+        return res.status(500).json({ message: "Error de servidor encontrado mientra agendaba la Cita.", error: error.message });
     }
         
         
