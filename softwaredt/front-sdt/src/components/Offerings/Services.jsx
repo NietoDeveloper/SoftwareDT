745c3276dde52962dc25a7d8c1c1fb6a3e7b4ed0
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-10 20:33:53 -0500

    Update Services.jsx

--- softwaredt/front-sdt/src/components/Offerings/Services.jsx
+++ softwaredt/front-sdt/src/components/Offerings/Services.jsx
@@ -8,7 +8,9 @@ const servicesList = [
 
   {
     name: "Soluciones para Empresas",
-    desc: "",
+    desc: "Software Contable: Automatiza registros financieros, genera balances, estados de resultados y cumple con normativas fiscales. Simplifica la contabilidad con herramientas para facturación, conciliación bancaria y reportes financieros.
+Manejo de Personal: Administra nóminas, calcula salarios, beneficios y deducciones. Controla horarios, asistencias y expedientes de empleados, optimizando la gestión de recursos humanos.
+Manejo de Usuarios e Información: Gestiona accesos y permisos de usuarios con seguridad. Organiza y protege datos empresariales, facilitando la búsqueda, almacenamiento y análisis de información clave.",
   },
 
   {
