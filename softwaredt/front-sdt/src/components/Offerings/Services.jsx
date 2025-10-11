142b7aeb407a7109f2dbf1205fbeaa67f0a9830c
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-10 20:04:00 -0500

    Update Services.jsx

--- softwaredt/front-sdt/src/components/Offerings/Services.jsx
+++ softwaredt/front-sdt/src/components/Offerings/Services.jsx
@@ -13,10 +13,7 @@ const servicesList = [
 
   {
     name: "Software Personalizado",
-    desc: "Solución diseñada a medida para satisfacer necesidades específicas de tu empresa. Incluye:
-
-Desarrollo a Medida: Funcionalidades adaptadas a procesos únicos, integrando requerimientos específicos de tu negocio.
-Escalabilidad: Flexible para crecer con tu empresa, permitiendo actualizaciones y nuevas integraciones.
+    desc: "Solución diseñada a medida para satisfacer necesidades específicas de tu empresa. Incluye: Desarrollo a Medida: Funcionalidades adaptadas a procesos únicos, integrando requerimientos específicos de tu negocio. Escalabilidad: Flexible para crecer con tu empresa, permitiendo actualizaciones y nuevas integraciones.
 Gestión de Datos: Almacena, organiza y protege información clave con interfaces personalizadas.
 Soporte Técnico: Asistencia continua para garantizar rendimiento y adaptabilidad. ",
   },
