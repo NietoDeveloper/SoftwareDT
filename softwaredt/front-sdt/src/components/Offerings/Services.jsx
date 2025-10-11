787ccd82c473445f5fd51e49df1b836752f493b2
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-10 20:31:44 -0500

    Update Services.jsx

--- softwaredt/front-sdt/src/components/Offerings/Services.jsx
+++ softwaredt/front-sdt/src/components/Offerings/Services.jsx
@@ -3,12 +3,14 @@ import ServicesCard from "./ServicesCard";
 const servicesList = [
   {
     name: "Desarrollo de Web, Apps y Bases de Datos",
-    desc: "",
+    desc: "Desarrollo Web: Creación de sitios web dinámicos, responsivos y optimizados, adaptados a tus necesidades, con diseño moderno y SEO integrado.
+Desarrollo de Apps: Aplicaciones móviles personalizadas para iOS y Android, con interfaces intuitivas y funcionalidades específicas para tu negocio.
+Bases de Datos: Diseño y gestión de bases de datos seguras, escalables y eficientes, optimizadas para almacenar, consultar y analizar información.",
   },
 
   {
     name: "Soluciones para Empresas",
-    desc: "Desarrollo Web: Creación de sitios web dinámicos, responsivos y optimizados, adaptados a tus necesidades, con diseño moderno y SEO integrado. Desarrollo de Apps: Aplicaciones móviles personalizadas para iOS y Android, con interfaces intuitivas y funcionalidades específicas para tu negocio. Bases de Datos: Diseño y gestión de bases de datos seguras, escalables y eficientes, optimizadas para almacenar, consultar y analizar información.",
+    desc: "",
   },
 
   {
