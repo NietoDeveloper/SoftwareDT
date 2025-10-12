382834047b664ff60ca3cceabd6249423fb4c120
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-11 19:00:21 -0500

    Update DoctorSignup.jsx

--- softwaredt/front-sdt/src/pages/DoctorSignup.jsx
+++ softwaredt/front-sdt/src/pages/DoctorSignup.jsx
@@ -22,7 +22,7 @@ const Doctorsignup = () => {
       <div className="flex flex-col md:flex-row gap-20">
         {/* Sign up icons */}
         <div className=" w-full md:w-1/2 p-6">
-          <h1 className="heading">Para Empleados SoftwareDT</h1>
+          <h1 className="heading">Para Empleados Software DT</h1>
           <p className="text_para">Ingresa como Programador</p>
           <p>Tienes Cuenta? <Link to="/doctor/login" className='text-blue-600'>Log in </Link> instead</p>
         </div>
