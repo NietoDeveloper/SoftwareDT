a9596adb6863aca51b4cd117cbb3f8e338c8ca6e
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-01 23:19:34 -0500

    Update Questions.jsx

--- softwaredt/front-sdt/src/components/Faq/Questions.jsx
+++ softwaredt/front-sdt/src/components/Faq/Questions.jsx
@@ -24,7 +24,7 @@ const Questions = () => {
               <div className="my-4 min-[700px]:my-6">
                 <FaqItem
                   question="¿Tiene algún costo la cita?"
-                  answer="No; La cita con un programador, es de tipo informativo y nos guiara en el servicio requerido. "
+                  answer="No;  La cita con un programador, es de tipo informativo y nos guiara en el servicio requerido. "
                   className="bg-transparent border border-gray-200 rounded-lg text-white"
                 />
               </div>
