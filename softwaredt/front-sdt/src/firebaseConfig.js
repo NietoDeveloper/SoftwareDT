7c35cc0087f875d95d96aeb953d6d22a40089d94
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-03 07:42:44 -0500

    Update firebaseConfig.js

--- softwaredt/front-sdt/src/firebaseConfig.js
+++ softwaredt/front-sdt/src/firebaseConfig.js
@@ -1,13 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyABiT0XyD68BJSaDBEaeYuake9TFwM0ggY",
-  authDomain: "appoinment-1acbb.firebaseapp.com",
-  projectId: "appoinment-1acbb",
-  storageBucket: "appoinment-1acbb.appspot.com",
-  messagingSenderId: "572447411053",
-  appId: "1:572447411053:web:cecb70662b71ec5bad6864",
-  measurementId: "G-MX184YVYNC"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app)
