1b36a8e6160dfbaa52dab0385f1540ec3bfb7478
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-07 07:22:53 -0500

    Update firebaseConfig.js

--- softwaredt/front-sdt/src/firebaseConfig.js
+++ softwaredt/front-sdt/src/firebaseConfig.js
@@ -1,20 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCAh9zfi_utySugSQI_qcW54xf67A4X0oU",
   authDomain: "software-dt.firebaseapp.com",
   projectId: "software-dt",
-  storageBucket: "software-dt.firebasestorage.app",
+  storageBucket: "software-dt-storage-alpha",
   messagingSenderId: "739796107685",
   appId: "1:739796107685:web:6652ea0c94a15b6aee684c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app)
-//const analytics = getAnalytics(app);
