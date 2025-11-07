6af2532a41b3fec094b6a16383ab82321cb2072b
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-07 01:06:06 -0500

    Update Doctors.jsx

--- softwaredt/front-sdt/src/pages/Doctors.jsx
+++ softwaredt/front-sdt/src/pages/Doctors.jsx
@@ -1,4 +1,4 @@
-import { axiosusers } from "../API/api";
+import { axiosAuth } from "../API/api";
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { BsArrowRight } from "react-icons/bs";
@@ -7,7 +7,7 @@ const DoctorList = () => {
   const navigate = useNavigate();
   const getDoctors = async () => {
     try {
-      const res = await axiosusers.get("/doctors");
+      const res = await axiosAuth.get("/doctors"); // Usar axiosAuth para peticiones públicas
       return res.data;
     } catch (error) {
       console.error("Error fetching doctors:", error);
@@ -31,53 +31,38 @@ const DoctorList = () => {
 
   return (
     <div className=" mx-auto px-4 py-8">
-      {" "}
       <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {" "}
         {doctors.length > 0 ? (
           doctors.map((doctor, index) => (
             <div
               key={index}
               className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow"
             >
-              {" "}
               <div className="flex flex-col items-center">
-                {" "}
                 <img
                   src="https://via.placeholder.com/150"
                   alt="profile"
                   className="w-full h-48 object-cover rounded-md mb-4"
                 />
-                {" "}
                 <h1 className="text-xl font-semibold mb-2">{doctor.name}</h1>
-                {" "}
                 <h2 className="text-gray-600 mb-2">{doctor.specialization}</h2>
-                {" "}
                 <p className="text-yellow-500 mb-4">
                   Puntaje: {doctor.totalRating}
                 </p>
-                {" "}
                 <p className="text-gray-700 mb-4">{doctor.bio}</p>
-                {" "}
                 <button
                   className="w-10 h-10 rounded-full border border-solid border-gray-900 flex items-center justify-center hover:bg-blue-600 transition-colors group"
                   onClick={() => handleDoctorClick(doctor._id)}
                 >
-                  {" "}
                   <BsArrowRight className="text-gray-900 group-hover:text-white transition-colors" />
-                  {" "}
                 </button>
-                {" "}
               </div>
-              {" "}
             </div>
           ))
         ) : (
           <p>No se encontro Programador</p>
         )}
-        {" "}
       </div>
-      {" "}
     </div>
   );
 };
