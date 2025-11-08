0dc87dabc79d10efad062eb1c32dc66cc3be52fb
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-07 23:59:06 -0500

    Update Contact.jsx

--- softwaredt/front-sdt/src/pages/Contact.jsx
+++ softwaredt/front-sdt/src/pages/Contact.jsx
@@ -142,8 +142,6 @@ const App = () => {
               </button>
             </form>
           </div>
-
-          {/* COLUMNA 3: Opciones y WhatsApp */}
           <div className="lg:col-span-1 space-y-8 mt-6 lg:mt-0">
             <div className="bg-green-50 border border-green-200 p-6 rounded-xl shadow-lg">
               <h3 className="text-xl font-bold text-gray-800 mb-4 flex items-center">
