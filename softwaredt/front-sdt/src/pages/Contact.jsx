c9a8037be7b2891894b5041bf07d05de16494e88
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-07 23:58:54 -0500

    Update Contact.jsx

--- softwaredt/front-sdt/src/pages/Contact.jsx
+++ softwaredt/front-sdt/src/pages/Contact.jsx
@@ -145,8 +145,6 @@ const App = () => {
 
           {/* COLUMNA 3: Opciones y WhatsApp */}
           <div className="lg:col-span-1 space-y-8 mt-6 lg:mt-0">
-            
-            {/* Tarjeta de WhatsApp */}
             <div className="bg-green-50 border border-green-200 p-6 rounded-xl shadow-lg">
               <h3 className="text-xl font-bold text-gray-800 mb-4 flex items-center">
                 <MessageCircleIcon className="w-6 h-6 text-green-600 mr-3" />
