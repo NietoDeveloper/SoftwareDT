a3b4d15111c04f98d92c4d5f613747fbeb3b0b52
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-09 05:01:22 -0500

    Update App.jsx

--- softwaredt/front-sdt/src/App.jsx
+++ softwaredt/front-sdt/src/App.jsx
@@ -1,50 +1,48 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Home from "./pages/Home";
+// Se añade la extensión .jsx o .js a todas las importaciones locales para resolver el error.
+import Header from "./components/Header/Header.jsx";
+import Home from "./pages/Home.jsx";
 import './App.css';
-import DoctorList from "./pages/Doctors";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login"
-import Doctorsignup from "./pages/DoctorSignup";
-import Doctorlogin from "./pages/DoctorLogin";
-import Services from "./pages/Services"; // ⬅️ ASUMIMOS QUE NECESITAS ESTO
-import {UserProvider} from './context/UserContext';
-import PrivateRoutes from "./utils/PrivateRoutes";
-import DoctorProfile from "./pages/BookingPage";
-import Contact from "./pages/Contact";
-import Payment from "./components/Checkout/Payment";
+import DoctorList from "./pages/Doctors.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx"
+import Doctorsignup from "./pages/DoctorSignup.jsx";
+import Doctorlogin from "./pages/DoctorLogin.jsx";
+import Services from "./pages/Services.jsx";
+import {UserProvider} from './context/UserContext.jsx';
+import PrivateRoutes from "./utils/PrivateRoutes.jsx";
+import DoctorProfile from "./pages/BookingPage.jsx";
+import Contact from "./pages/Contact.jsx";
+import Payment from "./components/Checkout/Payment.jsx";
 
 
 function App() {
   return (
-    
     <UserProvider>
-    <Header/>
-    
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/doctors" element={<DoctorList/>}/>
-      <Route path="/contact" element={<Contact/>}/>
+      <Header/>
       
-      {/* ⬅️ CORRECCIÓN: RUTA FALTANTE QUE CAUSA EL ERROR DE CONSOLA */}
-      <Route path="/services" element={<Services />} /> 
-      
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/login" element={<Login/>}/>
-      
-      <Route path="/doctor/signup" element={<Doctorsignup/>}/>
-      <Route path="/doctor/login" element={<Doctorlogin/>}/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/doctors" element={<DoctorList/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        
+        {/* Ruta para la página de Servicios */}
+        <Route path="/services" element={<Services />} /> 
+        
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
+        
+        <Route path="/doctor/signup" element={<Doctorsignup/>}/>
+        <Route path="/doctor/login" element={<Doctorlogin/>}/>
 
 
-      <Route element={<PrivateRoutes/>}>
-        <Route path="/doctors/:doctorId" element={<DoctorProfile/>}/>
-        <Route path="/checkout" element={<Payment/>}/>
-      </Route>
-
-      
-    </Routes>
+        {/* Rutas Protegidas */}
+        <Route element={<PrivateRoutes/>}>
+          <Route path="/doctors/:doctorId" element={<DoctorProfile/>}/>
+          <Route path="/checkout" element={<Payment/>}/>
+        </Route>
+      </Routes>
     </UserProvider>
-    
   )
 }
 
