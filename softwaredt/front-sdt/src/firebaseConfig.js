affc2259444350998f5a29644ee53fdbd577d131
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-09 20:38:25 -0500

    Update firebaseConfig.js

--- softwaredt/front-sdt/src/firebaseConfig.js
+++ softwaredt/front-sdt/src/firebaseConfig.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCAh9zfi_utySugSQI_qcW54xf67A4X0oU",
   authDomain: "software-dt.firebaseapp.com",
   projectId: "software-dt",
-  storageBucket: "software-dt.appspot.com", // SOLUCIONADO: Bucket de almacenamiento especificado
+  storageBucket: "software-dt.appspot.com",
   messagingSenderId: "739796107685",
   appId: "1:739796107685:web:6652ea0c94a15b6aee684c"
 };
