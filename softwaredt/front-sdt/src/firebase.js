0d07e490a3b459668f2f147ce74530727c7c755e
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-09 20:37:51 -0500

    Update firebase.js

--- softwaredt/front-sdt/src/firebase.js
+++ softwaredt/front-sdt/src/firebase.js
@@ -3,8 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Usar la configuración inyectada por el entorno para asegurar la integridad.
-// Se asume que '__firebase_config' es una variable global.
 const firebaseConfig = JSON.parse(typeof firebaseConfig !== 'undefined' ? firebaseConfig : '{}');
 
 let app;
