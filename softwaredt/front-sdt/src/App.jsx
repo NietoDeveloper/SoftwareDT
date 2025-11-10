ac0fcb0836000b5861d5385d333e4d12e69bf1c2
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-10 18:13:36 -0500

    Update App.jsx

--- softwaredt/front-sdt/src/App.jsx
+++ softwaredt/front-sdt/src/App.jsx
@@ -9,46 +9,50 @@ import Login from "./pages/Login.jsx"
 import Doctorsignup from "./pages/DoctorSignup.jsx";
 import Doctorlogin from "./pages/DoctorLogin.jsx";
 import Services from "./pages/Services.jsx";
-import {UserProvider} from './context/UserContext.jsx';
+// 🛑 AJUSTE DE RUTA: ASUMIENDO QUE App.jsx está en src/ y Context está en src/context/
+// y que el error anterior se debía a que App.jsx estaba en src/pages/ (¡pero ahora ya no!)
+import { UserProvider } from './context/UserContext.jsx'; 
+
 import PrivateRoutes from "./utils/PrivateRoutes.jsx";
 import DoctorProfile from "./pages/BookingPage.jsx";
 import Contact from "./pages/Contact.jsx";
 import Payment from "./components/Checkout/Payment.jsx";
 
 // ✨ Importación del nuevo Portal del Cliente (asumiendo que está en /pages)
-import ClientPortal from "./pages/ClientPortal.jsx";
+// AQUI TAMBIÉN ASUMIMOS QUE ES ClientePortal.jsx (con C mayúscula)
+import ClientPortal from "./pages/ClientPortal.jsx"; 
 
 
 function App() {
-  return (
-    <UserProvider>
-      <Header/>
-      
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/doctors" element={<DoctorList/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        
-        {/* Ruta para la página de Servicios */}
-        <Route path="/services" element={<Services />} /> 
-        
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        
-        <Route path="/doctor/signup" element={<Doctorsignup/>}/>
-        <Route path="/doctor/login" element={<Doctorlogin/>}/>
+    return (
+        <UserProvider>
+            <Header/>
+            
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/doctors" element={<DoctorList/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                
+                {/* Ruta para la página de Servicios */}
+                <Route path="/services" element={<Services />} /> 
+                
+                <Route path="/signup" element={<Signup/>}/>
+                <Route path="/login" element={<Login/>}/>
+                
+                <Route path="/doctor/signup" element={<Doctorsignup/>}/>
+                <Route path="/doctor/login" element={<Doctorlogin/>}/>
 
 
-        {/* Rutas Protegidas */}
-        <Route element={<PrivateRoutes/>}>
-          {/* 🎯 NUEVA RUTA: Portal del Cliente Protegida */}
-          <Route path="/portal" element={<ClientPortal/>}/> 
-          <Route path="/doctors/:doctorId" element={<DoctorProfile/>}/>
-          <Route path="/checkout" element={<Payment/>}/>
-        </Route>
-      </Routes>
-    </UserProvider>
-  )
+                {/* Rutas Protegidas */}
+                <Route element={<PrivateRoutes/>}>
+                    {/* 🎯 NUEVA RUTA: Portal del Cliente Protegida */}
+                    <Route path="/portal" element={<ClientPortal/>}/> 
+                    <Route path="/doctors/:doctorId" element={<DoctorProfile/>}/>
+                    <Route path="/checkout" element={<Payment/>}/>
+                </Route>
+            </Routes>
+        </UserProvider>
+    )
 }
 
 export default App;
