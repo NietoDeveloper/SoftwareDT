804098b373702fa52384be6d5e9791b352df81a1
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-12 11:39:23 -0500

    Update App.jsx

--- softwaredt/front-sdt/src/App.jsx
+++ softwaredt/front-sdt/src/App.jsx
@@ -35,13 +35,7 @@ function App() {
         {/* Rutas de autenticación de Doctor */}
         <Route path="/doctor/signup" element={<Doctorsignup/>}/>
         <Route path="/doctor/login" element={<Doctorlogin/>}/>
-
-        {/* Rutas Protegidas */}
-        <Route element={<PrivateRoutes/>}>
-          {/* Se utiliza BookingPage para el detalle del doctor y la reserva */}
-          <Route path="/doctors/:doctorId" element={<BookingPage/>}/>
-          <Route path="/checkout" element={<Payment/>}/>
-        </Route>
+        
                 {/* Rutas Protegidas */}
         <Route element={<PrivateRoutes/>}>
           {/* Se utiliza BookingPage para el detalle del doctor y la reserva */}
