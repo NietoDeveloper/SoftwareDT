528d2e9642422d064f5b768db90e00b55841c5e2
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-12 05:44:20 -0500

    Update App.jsx

--- softwaredt/front-sdt/src/App.jsx
+++ softwaredt/front-sdt/src/App.jsx
@@ -11,8 +11,6 @@ import Doctorlogin from "./pages/DoctorLogin.jsx";
 import Services from "./pages/Services.jsx";
 import {UserProvider} from './context/UserContext.jsx';
 import PrivateRoutes from "./utils/PrivateRoutes.jsx";
-// CORRECCIÓN: Se renombra la importación de DoctorProfile a BookingPage 
-// para reflejar su origen real (BookingPage.jsx)
 import BookingPage from "./pages/BookingPage.jsx";
 import Contact from "./pages/Contact.jsx";
 import Payment from "./components/Checkout/Payment.jsx";
