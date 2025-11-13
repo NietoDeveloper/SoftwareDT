a38abc82563ec613ed10e8588a9bae0002c710a1
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-13 03:23:22 -0500

    Update PrivateRoutes.jsx

--- softwaredt/front-sdt/src/utils/PrivateRoutes.jsx
+++ softwaredt/front-sdt/src/utils/PrivateRoutes.jsx
@@ -3,12 +3,11 @@ import { Outlet, Navigate } from "react-router-dom";
 import { AppContext } from "../context/UserContext";
 
 const PrivateRoutes = () => {
-    const {token} = useContext(AppContext);
+    const { token } = useContext(AppContext);
 
-    // Nota: El uso de 'replace: true' se escribe como 'replace'.
-    return (
-        token ? <Outlet/> : <Navigate to="/login" replace />
-    )
+    return (
+        token ? <Outlet /> : <Navigate to="/login" replace />
+    )
 }
 
 export default PrivateRoutes;
