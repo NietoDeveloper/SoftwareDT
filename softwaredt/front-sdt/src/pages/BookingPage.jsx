f40d62010404078d44529f51bfbcdb75a9d11397
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-13 08:06:09 -0500

    Update BookingPage.jsx

--- softwaredt/front-sdt/src/pages/BookingPage.jsx
+++ softwaredt/front-sdt/src/pages/BookingPage.jsx
@@ -2,10 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 // Se elimina la extensión '.jsx' del path para que el bundler pueda resolver el módulo correctamente.
 import { AppContext } from '../context/UserContext';
-// Importa tu instancia configurada de Axios
-// import { axiosAuth } from '../API/api'; 
 
-// --- Componentes SVG para Iconografía ---
 const CalendarIcon = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0H3" />
