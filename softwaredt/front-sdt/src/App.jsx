a6aeedcf5d6c9e604475019111f0c954ab2aa324
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-14 01:10:54 -0500

    Update App.jsx

--- softwaredt/front-sdt/src/App.jsx
+++ softwaredt/front-sdt/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+// 1. **ASUME** que el componente está en './pages/'. Corrige la ruta de importación.
+//    Si está en otro lugar, ajusta la ruta. Por ejemplo: "./pages/AppointmentConfirmation.jsx"
 import AppointmentConfirmation from "./pages/AppointmentConfirmation.jsx"; 
 import Header from "./components/Header/Header.jsx";
 import Home from "./pages/Home.jsx";
@@ -14,37 +16,46 @@ import PrivateRoutes from "./utils/PrivateRoutes.jsx";
 import BookingPage from "./pages/BookingPage.jsx";
 import Contact from "./pages/Contact.jsx";
 import Payment from "./components/Checkout/Payment.jsx";
-import MyAppointments from "./pages/MyAppointments.jsx";
 
 function App() {
-  return (
-    <UserProvider>
-      <Header />
+  return (
+    <UserProvider>
+      <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/doctors" element={<DoctorList />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/services" element={<Services />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<DoctorList />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
 
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/doctor/signup" element={<Doctorsignup />} />
-        <Route path="/doctor/login" element={<Doctorlogin />} />
+        {/* Rutas de autenticación */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/doctor/signup" element={<Doctorsignup />} />
+        <Route path="/doctor/login" element={<Doctorlogin />} />
 
-        <Route element={<PrivateRoutes />}>
-          <Route path="/book-appointment/:doctorId" element={<BookingPage />} />
-          <Route path="/checkout" element={<Payment />} />
-          <Route path="/my-appointments" element={<MyAppointments />} />
+        {/* Rutas Protegidas (Requieren autenticación) */}
+        <Route element={<PrivateRoutes />}>
+          {/* Cita de un Doctor específico */}
+          <Route path="/book-appointment/:doctorId" element={<BookingPage />} />
+          
+          {/* Página de Pago, movida a la ruta protegida si es necesario */}
+          <Route path="/checkout" element={<Payment />} />
 
-          <Route 
-            path="/appointment-confirmation/:appointmentId" 
-            element={<AppointmentConfirmation />} 
-          />
-        </Route>
-      </Routes>
-    </UserProvider>
-  );
+          {/* 2. LA RUTA DE CONFIRMACIÓN. La he movido dentro de PrivateRoutes por seguridad. */}
+          <Route 
+            path="/appointment-confirmation/:appointmentId" 
+            element={<AppointmentConfirmation />} 
+          />
+        </Route>
+
+        {/* 3. Ruta Duplicada Eliminada:
+            La ruta /book-appointment/:appointmentId con <BookingPage /> ya no es necesaria
+            y generaba conflicto con la anterior.
+        */}
+      </Routes>
+    </UserProvider>
+  );
 }
 
 export default App;
