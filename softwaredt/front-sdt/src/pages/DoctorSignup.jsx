5f4a281f27be5d075b08aa4588e311d8c8facdc9
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-15 22:18:21 -0500

    Update DoctorSignup.jsx

--- softwaredt/front-sdt/src/pages/DoctorSignup.jsx
+++ softwaredt/front-sdt/src/pages/DoctorSignup.jsx
@@ -3,7 +3,6 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios'; // Simulación de axiosAuth
 import { Link, useNavigate } from 'react-router-dom';
 
-// Componente de Icono de Registro (Lápiz en un documento)
 const RegisterIcon = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
         <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
