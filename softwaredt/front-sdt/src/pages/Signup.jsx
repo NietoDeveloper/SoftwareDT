355817bc77c90523cfef15ac74c5ba34f9c04f34
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-16 00:21:21 -0500

    Update Signup.jsx

--- softwaredt/front-sdt/src/pages/Signup.jsx
+++ softwaredt/front-sdt/src/pages/Signup.jsx
@@ -3,7 +3,6 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 
-// Componente de Icono de Usuario (para mayor claridad visual)
 const UserIcon = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
         <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
