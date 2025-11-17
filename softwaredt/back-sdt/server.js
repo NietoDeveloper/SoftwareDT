4ab22427ea7e449ea2655e598319ae93fa5d01a5
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-16 20:45:58 -0500

    Update server.js

--- softwaredt/back-sdt/server.js
+++ softwaredt/back-sdt/server.js
@@ -50,9 +50,6 @@ app.use('/api/user/review', require('./routes/reviewRoute'));
 app.use('/api/doctor/update', require('./routes/doctorRoutes/doctorUpdate'));
 app.use('/api/doctor/profile', require('./routes/bookingRoute'));
 
-// ----------------------------------------------------------------------
-
-
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
