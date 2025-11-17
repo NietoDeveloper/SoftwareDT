757e8815e071dfa229093e788861ad96f61cd635
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-17 01:22:26 -0500

    Update Login.jsx

--- softwaredt/front-sdt/src/pages/Login.jsx
+++ softwaredt/front-sdt/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import { AppContext } from '../context/UserContext.jsx';
