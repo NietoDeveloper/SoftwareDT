b8bd51e8f9ba71d84c844d9f6586311afdd47763
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-16 20:33:35 -0500

    Update Signup.jsx

--- softwaredt/front-sdt/src/pages/Signup.jsx
+++ softwaredt/front-sdt/src/pages/Signup.jsx
@@ -113,7 +113,6 @@ const Signup = () => {
                             {errors.name && <span className="text-red-600 text-sm mt-1 font-medium">{errors.name.message}</span>}
                         </div>
 
-                        {/* Campo Email */}
                         <div className="flex flex-col mb-4">
                             <label htmlFor="email" className="mb-2 font-medium text-gray-700 text-sm">Email</label>
                             <input
