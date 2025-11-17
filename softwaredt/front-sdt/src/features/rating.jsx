ca342e96ce27946352b753a6566131cb45f520be
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-16 20:39:38 -0500

    Update rating.jsx

--- softwaredt/front-sdt/src/features/rating.jsx
+++ softwaredt/front-sdt/src/features/rating.jsx
@@ -40,8 +40,6 @@ const ReviewForm = () => {
             />
             </div>
             
-
-            {/* Submit button */}
             <div className=''>
                  <button
               type="submit"
