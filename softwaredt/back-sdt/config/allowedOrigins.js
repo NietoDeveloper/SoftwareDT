69c8d105af7f6e8d1e8a3140fac5bdf11a27dd53
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-18 13:28:08 -0500

    Update allowedOrigins.js

--- softwaredt/back-sdt/config/allowedOrigins.js
+++ softwaredt/back-sdt/config/allowedOrigins.js
@@ -1,5 +1,7 @@
+// Este archivo debe ejecutarse después de que 'dotenv' haya cargado las variables.
 const allowedOrigins = [
-    'http://localhost:5173',
+    // Carga la URL del frontend desde la variable de entorno
+    process.env.FRONTEND_URL, 
 ];
 
 module.exports = allowedOrigins;
