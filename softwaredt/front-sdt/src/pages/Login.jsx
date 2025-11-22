d7512bcaee687305290f8a5535f16d2fd72ec4fc
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-22 18:29:16 -0500

    Update Login.jsx

--- softwaredt/front-sdt/src/pages/Login.jsx
+++ softwaredt/front-sdt/src/pages/Login.jsx
@@ -25,7 +25,6 @@ axiosPrivate.interceptors.request.use(
     }
 );
 
-// --- Componente Icono ---
 const LockIcon = (props) => (
     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
         <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
