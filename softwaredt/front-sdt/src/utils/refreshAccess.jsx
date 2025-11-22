4cb2997a0feef7206629bcb9686474d98857c186
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-22 18:24:23 -0500

    Update refreshAccess.jsx

--- softwaredt/front-sdt/src/utils/refreshAccess.jsx
+++ softwaredt/front-sdt/src/utils/refreshAccess.jsx
@@ -1,8 +1,6 @@
 import { axiosAuth } from "../API/api";
 
 /**
- * Función para solicitar un nuevo Access Token usando el Refresh Token (en cookies).
- * Esta función debe ser PURE (no debe usar setters de React ni hooks).
  * @returns {Promise<string>} El nuevo Access Token.
  * @throws {Error} 
  */
