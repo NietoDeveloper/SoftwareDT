c726969d5e4ddcfbfad3e529d5a74681b1f03368
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-23 14:41:56 -0500

    Update Doctors.jsx

--- softwaredt/front-sdt/src/pages/Doctors.jsx
+++ softwaredt/front-sdt/src/pages/Doctors.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { toast } from "react-toastify";
-import { axiosSecure } from "../utils/axiosSecure"; 
 
 const ArrowRightIcon = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
