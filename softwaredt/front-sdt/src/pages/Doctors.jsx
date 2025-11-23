63b83d6ae41c6a440479d27856fd7e9e0e2fd773
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-22 21:35:28 -0500

    Update Doctors.jsx

--- softwaredt/front-sdt/src/pages/Doctors.jsx
+++ softwaredt/front-sdt/src/pages/Doctors.jsx
@@ -14,6 +14,7 @@ const DoctorList = () => {
 
   const getDoctors = async () => {
     try {
+      // eslint-disable-next-line no-undef
       const res = await axiosSecure.get("/doctors"); 
       return res.data.doctors || res.data || [];
     } catch (error) {
