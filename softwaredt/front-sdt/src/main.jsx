0bc4be43ebed67152e3dd6fafd6f78f441c16a96
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-24 00:24:14 -0500

    Update main.jsx

--- softwaredt/front-sdt/src/main.jsx
+++ softwaredt/front-sdt/src/main.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import './firebaseConfig';
-import "react-toastify/dist/ReactToastify.css";
+import './firebaseConfig'; 
+import "react-toastify/dist/ReactToastify.css"; 
 import { ToastContainer } from 'react-toastify';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter 
+      future={{ 
+        v7_startTransition: true, 
+        v7_relativeSplatPath: true 
+      }}
+    >
       <QueryClientProvider client={queryClient}>
         <ToastContainer />
         <App />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
-);
+)
