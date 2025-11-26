478862dac5c3e49888599c7bfca175dca2296cd8
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-26 12:46:29 -0500

    Update Appointment.js

--- softwaredt/back-sdt/models/Appointment.js
+++ softwaredt/back-sdt/models/Appointment.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose
+const { Schema } = mongoose;
+
+const { citaDB } = require('../config/dbConn');
 
 const appointmentSchema = new Schema({
-    user:{type:mongoose.Types.ObjectId, ref:"User", required:true},
-    doctor:{type:mongoose.Types.ObjectId, ref:"Doctor", required:true},
-    appointmentDate: {
-        type: Date,
-        required: true,
-      },
-      status: {
+    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    doctor: { type: mongoose.Types.ObjectId, ref: "Doctor", required: true },
+    userInfo: { type: Object },  // Denormalized user data
+    doctorInfo: { type: Object },  // Denormalized doctor data
+    date: { type: String, required: true },  // e.g., 'YYYY-MM-DD'
+    time: { type: String, required: true },  // e.g., 'HH:MM'
+    status: {
         type: String,
         enum: ["pending", "approved", "cancelled"],
         default: "pending",
-      },
-    isPaid:{type:Boolean, default:false}
-}, {timestamps:true})
+    },
+    isPaid: { type: Boolean, default: false }
+}, { timestamps: true });
 
-module.exports = mongoose.model('Appointment', appointmentSchema);
+module.exports = citaDB.model('Appointment', appointmentSchema);
