71b40499f64417562f4cd8a568d6600b3c7bd5f9
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-27 17:22:15 -0500

    Update allDoctors.js

--- softwaredt/back-sdt/routes/allDoctors.js
+++ softwaredt/back-sdt/routes/allDoctors.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getAllDoctors } = require('../controllers/doctorController');  // Asegura controller usa Doctor.find({ isApproved: 'approved', isAvailable: true })
 
-// GET /api/user/doctors - Lista doctors disponibles (protegida por verifyAccess en server.js)
 router.get('/', getAllDoctors);
 
 module.exports = router;
