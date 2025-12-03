03b424285cc878c95216892acdec1edae88c3341
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-02 23:59:27 -0500

    Update Header.jsx

--- softwaredt/front-sdt/src/components/Header/Header.jsx
+++ softwaredt/front-sdt/src/components/Header/Header.jsx
@@ -16,13 +16,3 @@ const Header = () => {
     return (
         <header className="bg-white shadow-md sticky top-0 z-50">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between items-center h-16">
-                    
-                    <div className="flex-shrink-0 flex items-center cursor-pointer" onClick={() => navigate('/')}>
-                        <span className="text-black text-2xl font-bold">Software<span className="text-black">DT</span></span>
-                    </div>
-
-                    <nav className="hidden md:flex space-x-8 items-center">
-                        <Link to="/" className="text-black hover:text-yellow-500 font-medium transition-colors">Inicio</Link>
-                        <Link to="/doctors" className="text-black hover:text-yellow-500 font-medium transition-colors">Especialistas</Link>
-                        <Link to="/contact" className="text-black hover:text-yellow-500 font-medium transition-colors">Contacto</Link>
