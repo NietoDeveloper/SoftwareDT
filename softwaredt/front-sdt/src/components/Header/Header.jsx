d2748e931513305b680965a3b19622ebdd3bd491
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-02 23:52:57 -0500

    Update Header.jsx

--- softwaredt/front-sdt/src/components/Header/Header.jsx
+++ softwaredt/front-sdt/src/components/Header/Header.jsx
@@ -96,20 +96,3 @@ const Header = () => {
                         >
                             About
                         </Link>
-                        <Link 
-                            to="/appointment" 
-                            className="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-yellow-500 hover:bg-yellow-50 text-center transition-colors"
-                            onClick={() => setIsMenuOpen(false)}
-                        >
-                            Agenda Cita
-                        </Link>
-                        {isLoggedIn ? (
-                             <button 
-                                onClick={() => { handleLogout(); setIsMenuOpen(false); }}
-                                className="w-full text-left block px-3 py-2 rounded-md text-base font-medium text-black hover:bg-yellow-50 text-center transition-colors"
-                            >
-                                Cerrar Sesión
-                            </button>
-                        ) : (
-                            <>
-                                <Link 
