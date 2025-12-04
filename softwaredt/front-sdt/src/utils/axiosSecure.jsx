70e8a130f0da82e783515be80c1ca30e6990627a
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-03 20:43:55 -0500

    Update axiosSecure.jsx

--- softwaredt/front-sdt/src/utils/axiosSecure.jsx
+++ softwaredt/front-sdt/src/utils/axiosSecure.jsx
@@ -1,2 +1,36 @@
+import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1'; 
 
+const axiosSecure = axios.create({
+  baseURL: baseURL,
+  withCredentials: true, 
+});
+
+axiosSecure.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken'); 
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosSecure.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+      console.log('Error de autenticación detectado. Token expirado o no autorizado.');
+      
+    }
+    
+    return Promise.reject(error);
+  }
+);
+
+export default axiosSecure;
