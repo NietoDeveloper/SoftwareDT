487efae69538c45d419d988d96f10f7ad0cdb5e7
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-03 20:11:33 -0500

    Update Contact.jsx

--- softwaredt/front-sdt/src/pages/Contact.jsx
+++ softwaredt/front-sdt/src/pages/Contact.jsx
@@ -151,7 +151,7 @@ const Contact = () => {
             Gracias por tu feedback.
         </div>
       </section>
-      {/* FOOTER GLOBAL - Al final de la página */}
+
       <div className="pt-6 pb-6 bg-gray-50 border-t border-gray-200 text-center text-sm text-gray-500">
         © {new Date().getFullYear()} Software DT. Todos los derechos reservados.
       </div>
