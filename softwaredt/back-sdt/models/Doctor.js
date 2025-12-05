d46623ff58ba30b7e82cd559ec375e212840d556
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-05 03:37:34 -0500

    Update Doctor.js

---
+++ softwaredt/back-sdt/models/Doctor.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const { userDB } = require('../config/dbConn'); 
+
+const qualificationSchema = new Schema({
+  institution: { type: String, required: true },
+  degree: { type: String, required: true },
+  year: { type: Number, required: true }
+});
+
+const experienceSchema = new Schema({
+  hospital: { type: String, required: true },
+  role: { type: String, required: true },
+  startYear: { type: Number, required: true },
+  endYear: { type: Number }
+});
+
+const timeSlotSchema = new Schema({
+  day: { type: String, required: true },
+  startTime: { type: String, required: true },
+  endTime: { type: String, required: true }
+});
+
+const doctorSchema = new Schema({
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { 
+        type: String, 
+        required: true, 
+        select: false 
+    },
+    photo: { type: String },
+    phone: { type: String },
+    roles: {
+        doctor: {
+            type: Number,
+            default: 1001
+        },
+        admin: Number
+    },
+    ticketPrice: { type: Number, default: 30 },
+    specialization: { type: String, required: true },
+    qualifications: [qualificationSchema],
+    experience: [experienceSchema],
+    bio: { type: String, maxlength: 250 },
+    timeSlots: [timeSlotSchema],
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+    rating: { type: Number, default: 0 },
+    totalRating: { type: Number, default: 0 },
+    isApproved: { type: String, enum: ["pending", "approved", "cancelled"], default: "approved" },
+    appointments: [{ type: Schema.Types.ObjectId, ref: "Appointment" }],
+    refreshToken: {
+        type: [String],
+        select: false, 
+        index: true
+    },
+    isAvailable: { type: Boolean, default: true }
+}, { 
+    timestamps: true 
+});
+
+module.exports = userDB.model('Doctor', doctorSchema);
