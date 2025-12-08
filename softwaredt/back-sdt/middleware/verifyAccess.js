8a6c554b938e35a89e4d52325e24ffcba4105190
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-07 22:40:10 -0500

    Update verifyAccess.js

--- softwaredt/back-sdt/middleware/verifyAccess.js
+++ softwaredt/back-sdt/middleware/verifyAccess.js
@@ -4,7 +4,3 @@ const verifyAccess = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        console.log('DEBUG: Access Denied. Token is missing or invalid format (401).');
-        return res.status(401).json({ 
-            success: false, 
-            message: "Acceso denegado. Se requiere un token de portador ('Bearer')." 
