e5f365ad317322e7ce2d32a331d18e75c6fcdcb5
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-07 22:40:15 -0500

    Update verifyAccess.js

--- softwaredt/back-sdt/middleware/verifyAccess.js
+++ softwaredt/back-sdt/middleware/verifyAccess.js
@@ -1,6 +1,3 @@
 const jwt = require('jsonwebtoken');
 
 const verifyAccess = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
