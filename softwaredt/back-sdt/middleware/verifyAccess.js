e09e5753344b221758f3304a56c8c083f9913559
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-07 22:45:44 -0500

    Update verifyAccess.js

--- softwaredt/back-sdt/middleware/verifyAccess.js
+++ softwaredt/back-sdt/middleware/verifyAccess.js
@@ -41,7 +41,6 @@ const verifyAccess = (req, res, next) => {
                 });
             }
 
-            // 5. Verific
             if (!decoded || !decoded.id || !decoded.roles) {
                 console.log('DEBUG: Token payload is incomplete (missing user ID or roles).');
                 return res.status(403).json({ 
