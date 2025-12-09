b64718bf743b58753031b9aff954ba2860eebf98
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-08 22:34:50 -0500

    Update Doctor.js

--- softwaredt/back-sdt/models/Doctor.js
+++ softwaredt/back-sdt/models/Doctor.js
@@ -46,7 +46,7 @@ const doctorSchema = new Schema({
     bio: { type: String, maxlength: 250 },
     timeSlots: [timeSlotSchema],
     reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
-    rating: { type: Number, default: 0 },  // Posiblemente el promedio de ratings
+    rating: { type: Number, default: 0 },  
     totalRating: { type: Number, default: 0 },  // Posiblemente la suma o conteo de ratings
     isApproved: { type: String, enum: ["pending", "approved", "cancelled"], default: "approved" },
     appointments: [{ type: Schema.Types.ObjectId, ref: "Appointment" }],
