bff6a7b7d88097f398f4b370bd1e6ae2d03d027d
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-08 19:52:37 -0500

    Update allDoctors.js

--- softwaredt/back-sdt/routes/allDoctors.js
+++ softwaredt/back-sdt/routes/allDoctors.js
@@ -1,7 +1,7 @@
 // En tu archivo de ruta (ej. ./routes/allDoctors.js)
 const express = require('express');
 const router = express.Router();
-// 💡 CÓDIGO CORREGIDOcontrolado
+// 💡 
 const { getAllServices } = require('../controllers/serviceController'); 
 
 router.get('/', getAllServices); 
