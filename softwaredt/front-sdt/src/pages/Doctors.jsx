631dff47ea3bdace06173f5b4af5d864243ec298
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-08 21:52:31 -0500

    Update Doctors.jsx

--- softwaredt/front-sdt/src/pages/Doctors.jsx
+++ softwaredt/front-sdt/src/pages/Doctors.jsx
@@ -87,12 +87,3 @@ const DoctorList = () => {
             
             <div className="w-10 h-10 rounded-full border border-solid border-gray-900 flex items-center justify-center bg-transparent group-hover:bg-blue-600 transition-colors mt-auto cursor-pointer">
               <ArrowRightIcon className="text-gray-900 group-hover:text-white transition-colors" />
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default DoctorList;
