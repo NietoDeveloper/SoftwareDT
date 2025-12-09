03d422c4d40410b5159996e56119c9034604f79a
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-08 22:17:00 -0500

    Update server.js

--- softwaredt/back-sdt/server.js
+++ softwaredt/back-sdt/server.js
@@ -3,29 +3,3 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose'); 
-
-const { userDB, citaDB } = require('./config/dbConn'); 
-
-const corsOptions = require('./config/corsOptions');
-const {verifyAccess} = require('./middleware/verifyAccess');
-const {unknownEndpoint} = require('./middleware/notFound');
-const {errorHandler} = require('./middleware/errorHandler');
-const path = require('path');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'Server Operational',
-        message: 'Welcome to the API root.'
-    });
-});
-
-app.use('/api/user/register', require('./routes/userRoutes/userRegister'));
-
