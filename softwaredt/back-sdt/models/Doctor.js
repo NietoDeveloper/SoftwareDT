0fc39714388c74be3b44eac117e498d1c129d3be
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-08 22:34:01 -0500

    Update Doctor.js

--- softwaredt/back-sdt/models/Doctor.js
+++ softwaredt/back-sdt/models/Doctor.js
@@ -1,26 +1,4 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-const { userDB } = require('../config/dbConn'); 
-
-const qualificationSchema = new Schema({
-  institution: { type: String, required: true },
-  degree: { type: String, required: true },
-  year: { type: Number, required: true }
-});
-
-const experienceSchema = new Schema({
-  hospital: { type: String, required: true },
-  role: { type: String, required: true },
-  startYear: { type: Number, required: true },
-  endYear: { type: Number }  // Opcional, ya que podría ser actual
-});
-
-const timeSlotSchema = new Schema({
-  day: { type: String, required: true },
-  startTime: { type: String, required: true },
-  endTime: { type: String, required: true }
-});
 
 const doctorSchema = new Schema({
     email: { type: String, required: true, unique: true },
