1eaef7af4b3f01ce4d1cbb30b564acc5f945e8da
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-08 22:14:30 -0500

    Update Doctor.js

--- softwaredt/back-sdt/models/Doctor.js
+++ softwaredt/back-sdt/models/Doctor.js
@@ -3,14 +3,6 @@
   endTime: { type: String, required: true }
 });
 
-const doctorSchema = new Schema({
-    email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    password: { 
-        type: String, 
-        required: true, 
-        select: false 
-    },
     photo: { type: String },
     phone: { type: String },
     roles: {
