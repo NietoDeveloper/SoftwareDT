b277429f8f8cbead44f17e2c251a358c3e1f2002
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-08 22:14:36 -0500

    Update Doctor.js

--- softwaredt/back-sdt/models/Doctor.js
+++ softwaredt/back-sdt/models/Doctor.js
@@ -1,17 +1,5 @@
 
   startTime: { type: String, required: true },
-  endTime: { type: String, required: true }
-});
-
-    photo: { type: String },
-    phone: { type: String },
-    roles: {
-        doctor: {
-            type: Number,
-            default: 1001
-        },
-        admin: Number
-    },
-    ticketPrice: { type: Number, default: 30 },
+  
     specialization: { type: String, required: true },
     qualifications: [qualificationSchema],
