c4a1653aa1b09ca43282d81bc12bbb9359dfb9fb
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-08 19:52:22 -0500

    Update allDoctors.js

--- softwaredt/back-sdt/routes/allDoctors.js
+++ softwaredt/back-sdt/routes/allDoctors.js
@@ -1,7 +1,7 @@
 // En tu archivo de ruta (ej. ./routes/allDoctors.js)
 const express = require('express');
 const router = express.Router();
-// 💡 CÓDIGO CORREGIDO: Usar el nombre del controlador que coincide con la funcionalidad
+// 💡 CÓDIGO CORREGIDOcontrolador que coincide con la funcionalidad
 const { getAllServices } = require('../controllers/serviceController'); 
 
 router.get('/', getAllServices); 
