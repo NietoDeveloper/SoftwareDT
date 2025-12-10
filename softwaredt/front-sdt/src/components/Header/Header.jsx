7ec2a8a3679a3f880862d522c34cdca1f48a4e2e
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-09 21:46:50 -0500

    Update Header.jsx

--- softwaredt/front-sdt/src/components/Header/Header.jsx
+++ softwaredt/front-sdt/src/components/Header/Header.jsx
@@ -74,7 +74,7 @@ const Header = () => {
                             Inicio
                         </Link>
                         <Link
-                            to="/# <Services />"
+                            to="/# Services"
                             className="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-yellow-500 hover:bg-yellow-50 text-center transition-colors"
                             onClick={() => setIsMenuOpen(false)}
                         >
