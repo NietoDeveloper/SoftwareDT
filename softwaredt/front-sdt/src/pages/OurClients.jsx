bd527c2de1baf5e033712c9eda14664a52ee1fb2
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-09 22:17:27 -0500

    Update OurClients.jsx

--- softwaredt/front-sdt/src/pages/OurClients.jsx
+++ softwaredt/front-sdt/src/pages/OurClients.jsx
@@ -6,7 +6,7 @@ const clientData = [
     id: 1,
     title: "Cliente Alpha S.A.S.",
     description: "Desarrollo de plataforma de gestión interna y optimización de flujos de trabajo.",
-    imageUrl: "/images/client-alpha.png", // <--- Reemplace con la ruta real
+    imageUrl: "/images/client-alpha.png", // <--- Reemplace 
     website: "https://www.clientealpha.com",
   },
   {
