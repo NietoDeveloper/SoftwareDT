5a2fcaf3cdd3a7cb02542acb67b3bd293c7531a0
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-09 22:17:57 -0500

    Update OurClients.jsx

--- softwaredt/front-sdt/src/pages/OurClients.jsx
+++ softwaredt/front-sdt/src/pages/OurClients.jsx
@@ -13,7 +13,7 @@ const clientData = [
     id: 2,
     title: "Firma Beta Legal",
     description: "Creación de un portal de consulta legal con documentación organizada.",
-    imageUrl: "/images/client-beta.png", // <--- Reemplace con la 
+    imageUrl: "/images/client-beta.png", // <--- Reemp
     website: "https://www.firmabeta.com",
   },
   {
