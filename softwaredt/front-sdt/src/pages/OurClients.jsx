04ae6ad77f1da0733aac16be5008c057bb71199b
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-09 22:18:28 -0500

    Update OurClients.jsx

--- softwaredt/front-sdt/src/pages/OurClients.jsx
+++ softwaredt/front-sdt/src/pages/OurClients.jsx
@@ -20,7 +20,7 @@ const clientData = [
     id: 3,
     title: "Tech Gamma Solutions",
     description: "Integración de API para servicios de terceros y microservicios.",
-    imageUrl: "/images/client-gamma.png", // <
+    imageUrl: "/images/client-gamma.png",
     website: "https://www.techgamma.com",
   },
   {
