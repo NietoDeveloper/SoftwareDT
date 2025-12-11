034b8bf62d8acddd3bb7d20167e7745d0998ce61
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-10 22:44:00 -0500

    Update dbConn.js

--- back-sdt/config/dbConn.js
+++ back-sdt/config/dbConn.js
@@ -25,7 +25,7 @@ const initializeConnection = (uri, name) => { // 1. Quitar 'dbName' como argumen
     return connection;
 };
 
-// 3. Llamar a la función sin el tercer argumento (dbName)
+// 3. 
 
 const userDB = initializeConnection(
     process.env.MONGODB_URI_USUARIOS, 
