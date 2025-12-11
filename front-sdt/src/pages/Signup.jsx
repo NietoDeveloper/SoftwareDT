571973a519fb6e933379ff6c54967a9d6032df6b
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-10 22:52:03 -0500

    Update Signup.jsx

--- front-sdt/src/pages/Signup.jsx
+++ front-sdt/src/pages/Signup.jsx
@@ -29,7 +29,6 @@ const validateField = (field, value) => {
 };
 
 const App = () => {
-    // Estado para los datos del formulario 
     const [formData, setFormData] = useState({
         name: '',
         email: '',
