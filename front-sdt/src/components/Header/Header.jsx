44b791ef55a051cd3bce803077121058e6c79559
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 15:56:08 -0500

    Update Header.jsx

--- front-sdt/src/components/Header/Header.jsx
+++ front-sdt/src/components/Header/Header.jsx
@@ -55,26 +55,7 @@ const Header = () => {
                             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 {isMenuOpen ? (
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                                ) : (
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                                )}
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            {isMenuOpen && (
-                <div className={`md:hidden bg-transparent backdrop-blur-3xl border-t border-gray-100 max-h-[50vh] overflow-auto transition-all duration-300 ease-in-out ${isMenuOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4'}`}>
-                    <div className="px-2 pt-2 pb-3 space-y-0.5 sm:px-3">
-                        <Link
-                            to="/"
-                            className="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-yellow-500 hover:bg-yellow-50 text-center transition-colors"
-                            onClick={() => setIsMenuOpen(false)}
-  
 
- 
-                        )}
-                    </div>
                 </div>
             )}
         </header>
