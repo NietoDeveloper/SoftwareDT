dcbe830ac188dc2361fd23962d973d7cffe994e5
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 15:56:48 -0500

    Update Header.jsx

--- front-sdt/src/components/Header/Header.jsx
+++ front-sdt/src/components/Header/Header.jsx
@@ -5,9 +5,7 @@ const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const navigate = useNavigate();
 
-    const isLoggedIn = !!localStorage.getItem('token');
 
-    const handleLogout = () => {
 
     );
 };
