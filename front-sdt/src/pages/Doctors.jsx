48112a29fadf90634fe2750993a553657603d290
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 18:12:45 -0500

    Update Doctors.jsx

--- front-sdt/src/pages/Doctors.jsx
+++ front-sdt/src/pages/Doctors.jsx
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { toast } from "react-toastify";
-import axios from "axios";  
+import axios from "axios"; 
+import Footer from "../components/Footer/Footer"; 
 
 const ArrowRightIcon = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
@@ -86,7 +87,6 @@ const DoctorList = () => {
           </div>
         ))}
       </div>
-      <Footer />
     </div>
   );
 };
