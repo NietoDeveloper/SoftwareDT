041cdd87523382ce099229a37dd022363f495994
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 15:56:53 -0500

    Update Header.jsx

--- front-sdt/src/components/Header/Header.jsx
+++ front-sdt/src/components/Header/Header.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const navigate = useNavigate();
+
 
 
 
