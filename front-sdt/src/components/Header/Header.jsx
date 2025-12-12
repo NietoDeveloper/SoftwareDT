a6a44e3afd5f8fb70e0d3895581ad15cbdb8e4aa
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 16:10:23 -0500

    Update Header.jsx

--- front-sdt/src/components/Header/Header.jsx
+++ front-sdt/src/components/Header/Header.jsx
@@ -14,24 +14,7 @@ const Header = () => {
     };
 
     return (
-        <header className="bg-transparent backdrop-blur-3xl shadow-lg sticky top-0 z-50">
-
-            </div>
-            {isMenuOpen && (
-                <div className={`md:hidden bg-transparent backdrop-blur-3xl border-t border-gray-100 max-h-[50vh] overflow-auto transition-all duration-300 ease-in-out ${isMenuOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4'}`}>
-                    <div className="px-2 pt-2 pb-3 space-y-0.5 sm:px-3">
-
-
-
-
-                        {isLoggedIn ? (
-                             <button
-                                onClick={() => { handleLogout(); setIsMenuOpen(false); }}
-                                className="w-full block px-3 py-2 rounded-md text-base font-medium text-black hover:text-yellow-500 hover:bg-yellow-50 text-center transition-colors"
-                            >
-                                Cerrar Sesión
-                            </button>
-                        ) : (
+       
                             <>
                                
  
