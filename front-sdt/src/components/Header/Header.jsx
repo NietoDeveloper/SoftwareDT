dd2c0e84b9c0388795817f55fc57d6f2b1ee2f39
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 16:09:07 -0500

    Update Header.jsx

--- front-sdt/src/components/Header/Header.jsx
+++ front-sdt/src/components/Header/Header.jsx
@@ -80,13 +80,7 @@ const Header = () => {
                         >
                             Informacion Servicios
                         </Link>
-                        <Link
-                            to="/doctors"
-                            className="block px-3 py-2 rounded-md text-base font-medium text-black hover:text-yellow-500 hover:bg-yellow-50 text-center transition-colors"
-                            onClick={() => setIsMenuOpen(false)}
-                        >
-                            Escoje Servicio
-                        </Link>
+
 
 
                         {isLoggedIn ? (
