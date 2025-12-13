d9b37cc481fc689789bbef183b933d1f31796443
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 20:09:21 -0500

    Update OurClients.jsx

--- front-sdt/src/pages/OurClients.jsx
+++ front-sdt/src/pages/OurClients.jsx
@@ -35,21 +35,6 @@ const clientData = [
 ];
 
 const OurClients = () => {
-  return (
-    <div>
-    <section className="flex flex-col items-center p-4 sm:p-8 md:p-12 min-h-screen">
-      <h2 className="text-4xl sm:text-5xl font-extrabold text-black mb-12 text-center">
-        Nuestros Clientes
-      </h2>
 
-      <div className="w-full max-w-[1400px]">
-
-        </div>
-      </div>
-    </section>
-     <Footer />
-    </div>
-  );
-};
 
 export default OurClients;
