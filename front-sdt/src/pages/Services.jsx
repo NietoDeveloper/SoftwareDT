ee6b7786ec10b4ee3d97901233c0bb23fd79eb8c
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 20:41:51 -0500

    Update Services.jsx

--- front-sdt/src/pages/Services.jsx
+++ front-sdt/src/pages/Services.jsx
@@ -2,19 +2,7 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
 
-const ServicesList = () => {
-  const functionalPlaceholder =
-    "https://placehold.co/150x150/EEEEEE/313131?text=Servicio";
 
-  const services = [
-    {
-      title: "Desarrollo Web, Apps y Bases de Datos",
-
-
-
-  ];
-
-  return (
 
 
 
