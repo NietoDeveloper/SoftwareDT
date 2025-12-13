8d8d936f15b33f7861c37ed79dc8ae8d27503eb3
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 20:46:14 -0500

    Update Services.jsx

--- front-sdt/src/pages/Services.jsx
+++ front-sdt/src/pages/Services.jsx
@@ -24,22 +24,7 @@ const ServicesList = () => {
       photo: functionalPlaceholder,
       price: "Desde  $ 8.000.000 COP",
     },
-    {
-      title: "Mantenimiento y Soporte IT",
-      subtitle: "Continuidad del negocio",
-      description:
-        "Soporte técnico continuo, monitorización de servidores, actualizaciones de seguridad y mantenimiento preventivo para garantizar la operatividad de tus sistemas.",
-      photo: functionalPlaceholder,
-      price: "Desde  $ 8.000.000 COP",
-    },
-    {
-      title: "Diseño de Interfaz (UI/UX)",
-      subtitle: "Experiencia de usuario inmersiva",
-      description:
-        "Diseño centrado en el usuario, creación de prototipos y pruebas de usabilidad para garantizar que tu producto sea intuitivo, accesible y atractivo.",
-      photo: functionalPlaceholder,
-      price: "Desde  $ 3.000.000 COP",
-    },
+
 
   ];
 
