6b509645579a42831bcf0ff61fe4a8918683d529
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-12 19:36:23 -0500

    Update OurClients.jsx

--- front-sdt/src/pages/OurClients.jsx
+++ front-sdt/src/pages/OurClients.jsx
@@ -49,6 +49,7 @@ const clientData = [
 
 const OurClients = () => {
   return (
+    <div>
     <section className="flex flex-col items-center p-4 sm:p-8 md:p-12 min-h-screen">
       <h2 className="text-4xl sm:text-5xl font-extrabold text-black mb-12 text-center">
         Nuestros Clientes
@@ -93,6 +94,7 @@ const OurClients = () => {
         </div>
       </div>
     </section>
+    </div>
   );
 };
 
