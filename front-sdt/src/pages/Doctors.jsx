957879a4c27cabbe444a8e929625b5e434f0badc
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-13 20:30:40 -0500

    Update Doctors.jsx

--- front-sdt/src/pages/Doctors.jsx
+++ front-sdt/src/pages/Doctors.jsx
@@ -2,12 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import axios from "axios"; 
-import Footer from "../components/Footer/Footer"; 
 
-
-
-const DoctorList = () => {
-  const navigate = useNavigate();
 
 
 
