573afb288cde347f35e3a7ce5a0c0ecc738e1181
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-13 20:30:12 -0500

    Update Doctors.jsx

--- front-sdt/src/pages/Doctors.jsx
+++ front-sdt/src/pages/Doctors.jsx
@@ -26,12 +26,7 @@ const DoctorList = () => {
             className="group bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition-shadow cursor-pointer flex flex-col items-center text-center h-[300px]"
             onClick={() => navigateToBooking(doctor._id)}
           >
-            <h1 className="text-xl font-semibold mb-2">{doctor.name}</h1>
-            <h2 className="text-gray-600 mb-2">{doctor.specialization}</h2>
-            <p className="text-yellow-500 mb-4">
-              Puntaje: {doctor.totalRating}
-            </p>
-            <p className="text-gray-700 mb-4 line-clamp-2 flex-grow overflow-hidden">{doctor.bio}</p>
+
 
         ))}
       </div>
