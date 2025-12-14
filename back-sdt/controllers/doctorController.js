c51db1fd091951f0ce1b2f1e671cfdf1acc95d6e
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-13 20:34:07 -0500

    Update doctorController.js

--- back-sdt/controllers/doctorController.js
+++ back-sdt/controllers/doctorController.js
@@ -1,18 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const Doctor = require('../models/Doctor');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 
-const doctorRegister = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
-
-    if (!name || !email || !password) {
-        return res.status(400).json({ message: "Name, password and email are required!" });
-    }
-    const duplicate = await Doctor.findOne({ email });
-    if (duplicate) {
-        return res.status(409).json({ message: "Email already exists" }); 
-    }
 
 
 
