998450197c3fa527bbd3b6a893ded41eea186f84
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-13 20:34:00 -0500

    Update doctorController.js

--- back-sdt/controllers/doctorController.js
+++ back-sdt/controllers/doctorController.js
@@ -14,20 +14,7 @@ const doctorRegister = asyncHandler(async (req, res) => {
         return res.status(409).json({ message: "Email already exists" }); 
     }
 
-    const hashedpassword = await bcrypt.hash(password, 10);
-    const result = await Doctor.create({
-        name, 
-        email,
-        password: hashedpassword,
-        roles: { doctor: 1001 } 
-    });
 
-    if (result) {
-        res.status(201).json({ message: "Your doctor's profile was created successfully" });
-    } else {
-        res.status(500).json({ message: "Internal server error: Could not create profile." }); 
-    }
-});
 
 
 
