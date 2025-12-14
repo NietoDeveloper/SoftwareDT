1d8cb1bd475cd67df809c17875d432eced4761a2
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-13 20:22:41 -0500

    Update Doctors.jsx

--- front-sdt/src/pages/Doctors.jsx
+++ front-sdt/src/pages/Doctors.jsx
@@ -38,10 +38,7 @@ const DoctorList = () => {
 
   if (isLoading) return <h1 className="text-center py-10 text-xl font-bold">Cargando Servicios....</h1>;
   
-  if (error) {
-    const errorMessage = error.response ? `HTTP ${error.response.status}: ${error.response.data.message || 'Error desconocido.'}` : error.message;
-    return <h1 className="text-center py-10 text-red-600 text-xl font-bold">Error cargando los Datos. {errorMessage}</h1>;
-  }
+
 
 
 
