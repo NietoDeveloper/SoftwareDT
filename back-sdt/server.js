1571028fb6aa2ee6c0cdfd1e6056b454646cdca5
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-14 22:18:04 -0500

    Update server.js

--- back-sdt/server.js
+++ back-sdt/server.js
@@ -10,12 +10,6 @@ const corsOptions = require('./config/corsOptions');
 
 
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-
 
 
 
