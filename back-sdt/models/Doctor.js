22601fda4b10753c9d532925a86c7d87b820c78a
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-14 20:04:20 -0500

    Update Doctor.js

--- back-sdt/models/Doctor.js
+++ back-sdt/models/Doctor.js
@@ -1,12 +1,1 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const { userDB } = require('../config/dbConn'); 
-
-
-
-
-
-
-
-module.exports = userDB.model('Doctor', doctorSchema);
