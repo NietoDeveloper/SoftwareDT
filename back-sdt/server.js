232d3b88cf4904c79748a3cbea0435fcbe7f88b9
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-14 22:18:28 -0500

    Update server.js

--- back-sdt/server.js
+++ back-sdt/server.js
@@ -5,23 +5,3 @@ const express = require('express');
 
 
 
-
-
-
-
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-
-
-Promise.all([
-
-]).then(() => {
-
-
-}).catch(err => {
-
-});
