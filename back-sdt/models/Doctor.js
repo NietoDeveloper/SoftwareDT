73781d802eee4b3c339455a5b341850e7924f3f6
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-14 20:04:14 -0500

    Update Doctor.js

--- back-sdt/models/Doctor.js
+++ back-sdt/models/Doctor.js
@@ -3,11 +3,7 @@ const { Schema } = mongoose;
 
 const { userDB } = require('../config/dbConn'); 
 
-const timeSlotSchema = new Schema({
-  day: { type: String, required: true },
-  startTime: { type: String, required: true },
-  endTime: { type: String, required: true }
-});
+
 
 
 
