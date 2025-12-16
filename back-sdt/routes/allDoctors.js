8b730ac6ba349c488cb2f7e491ddadcdb34325f6
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-15 20:17:36 -0500

    Update allDoctors.js

--- back-sdt/routes/allDoctors.js
+++ back-sdt/routes/allDoctors.js
@@ -5,10 +5,6 @@ const { getAllDoctors } = require('../controllers/doctorController');
 
 router.get('/', (req, res, next) => {
 
-    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    res.set('Pragma', 'no-cache');
-    res.set('Expires', '0');
-    res.set('ETag', ''); 
     
     next(); 
 }, getAllDoctors); 
