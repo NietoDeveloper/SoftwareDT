4d2a438a123a4fa4bc00657d4353b411ccef38b8
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-16 01:33:44 -0500

    Update allDoctors.js

--- back-sdt/routes/allDoctors.js
+++ back-sdt/routes/allDoctors.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const { getAllDoctors } = require('../controllers/doctorController'); 
 
-// Esta es la forma estándar y limpia de definir una ruta GET que llama a un controlador.
-// La ruta completa es ahora más directa: GET /api/doctors/ (o la ruta que uses en server.js)
 router.get('/', getAllDoctors); 
 
 module.exports = router;
