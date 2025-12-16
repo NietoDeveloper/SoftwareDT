df6c14bbbc972246af46327dfeb89373a26de779
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-15 20:17:43 -0500

    Update allDoctors.js

--- back-sdt/routes/allDoctors.js
+++ back-sdt/routes/allDoctors.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllDoctors } = require('../controllers/doctorController'); 
-
-router.get('/', (req, res, next) => {
 
     
     next(); 
