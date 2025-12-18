7fda81f10e6581b10f42c180c1104c7181cf054e
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-17 22:28:22 -0500

    Update appointmentController.js

--- back-sdt/controllers/appointmentController.js
+++ back-sdt/controllers/appointmentController.js
@@ -1,27 +1,3 @@
-const Appointment = require('../models/Appointment');
-const User = require('../models/User');
-const asyncHandler = require('express-async-handler');
-
-const appointmentBooking = asyncHandler(async (req, res) => {
-    const doctor = req?.params?.id;
-    const { appointmentDate } = req.body;
-
-    if (!doctor || !appointmentDate) {
-        return res.status(400).json({ message: "Doctor and appointment date are required!" });
-    }
-
-    const userId = req.userId;
-
-    try {
-        const newAppointment = await Appointment.create({
-            user: userId,
-            doctor,
-            appointmentDate,
-        });
-
-        if(newAppointment) {
-
-
 
         
 });
