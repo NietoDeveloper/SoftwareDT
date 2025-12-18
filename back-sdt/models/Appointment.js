4f31f5d58390641e69d9dc40b48d80cdf71e4c01
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-17 22:30:12 -0500

    Update Appointment.js

--- back-sdt/models/Appointment.js
+++ back-sdt/models/Appointment.js
@@ -4,13 +4,7 @@ const { Schema } = mongoose;
 const { citaDB } = require('../config/dbConn');
 
 const appointmentSchema = new Schema({
-    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
-    doctor: { type: mongoose.Types.ObjectId, ref: "Doctor", required: true },
-    userInfo: { type: Object },  // Denormalized user data
-    doctorInfo: { type: Object },  // Denormalized doctor data
-    date: { type: String, required: true },  // e.g., 'YYYY-MM-DD'
-    time: { type: String, required: true },  // e.g., 'HH:MM'
-    status: {
+
         type: String,
         enum: ["pending", "approved", "cancelled"],
         default: "pending",
