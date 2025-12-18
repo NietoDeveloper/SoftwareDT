1416f33d9caa822b15709bbbebbb747576f06993
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-17 22:30:25 -0500

    Update Appointment.js

--- back-sdt/models/Appointment.js
+++ back-sdt/models/Appointment.js
@@ -3,12 +3,6 @@ const { Schema } = mongoose;
 
 const { citaDB } = require('../config/dbConn');
 
-const appointmentSchema = new Schema({
-
-        type: String,
-        enum: ["pending", "approved", "cancelled"],
-        default: "pending",
-    },
 
 
 module.exports = citaDB.model('Appointment', appointmentSchema);
