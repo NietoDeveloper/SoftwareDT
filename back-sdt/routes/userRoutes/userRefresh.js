9ca3dab56e6c30ab42ae5b4fe40cf30c1ca0b3a5
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-17 23:56:57 -0500

    ok

--- back-sdt/routes/userRoutes/userRefresh.js
+++ back-sdt/routes/userRoutes/userRefresh.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { handleRefreshToken } = require('../../controllers/refreshTokenController');
 
-router.get('/', handleRefreshToken)
+// Cambiado a POST para recibir el token en el req.body desde el frontend
+router.post('/', handleRefreshToken);
 
-module.exports = router
+module.exports = router;
