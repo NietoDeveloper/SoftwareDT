4850bfe672f5fe661667d7a921099de9541736fc
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-20 16:38:59 -0500

    Update Herosection.jsx

--- front-sdt/src/components/Hero/Herosection.jsx
+++ front-sdt/src/components/Hero/Herosection.jsx
@@ -15,20 +15,7 @@ const Herosection = () => {
                    text-gray-800 font-[600] lg:text-[56px] lg:leading-[64px] mb-6"
             >
               Construimos Software Que Da Solucion
-            </h1>
-            <p className="text_para mb-8 text-lg text-gray-600">
-              Innovamos con software de vanguardia. Transformamos negocios con
-              tecnología personalizada. Potenciamos empresas con herramientas
-              digitales. Diseñamos apps, web y software a medida. Soluciones
-              escalables, intuitivas y eficientes. Con Tu visión y nuestra
-              tecnología, resultados reales. Eleva tu negocio con nosotros.
-            </p>
-            <Link to="/doctors">
-              <button className="px-8 py-3 text-lg font-medium text-white border-2 border-transparent rounded-xl transition duration-300 bg-blue-600 hover:bg-yellow-400 hover:text-black">
-                Agenda Tu Cita
-              </button>
-            </Link>
-          </div>
+
 
           <div className="w-full lg:w-1/2 flex justify-center lg:justify-end z-10">
             <div className="flex flex-col gap-5 max-w-[350px] lg:max-w-full">
