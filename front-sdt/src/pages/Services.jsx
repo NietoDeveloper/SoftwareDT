51826440402d28ad902becc2fbe6f648c0b2ff9f
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-20 12:32:09 -0500

    Update Services.jsx

--- front-sdt/src/pages/Services.jsx
+++ front-sdt/src/pages/Services.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
 
-// Icono optimizado para mantener coherencia visual
 const ArrowRightIcon = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
     <line x1="5" y1="12" x2="19" y2="12"></line>
