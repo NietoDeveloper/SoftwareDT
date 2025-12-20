7a3f47343b7b04831d383a2bd04e8e3fa1f31fa3
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-20 15:26:12 -0500

    Update Home.jsx

--- front-sdt/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Herosection from "../components/Hero/Herosection";
-import About from "../components/About";
-import Services from "../components/Offerings/Services";
-import Medcare from "../components/Preview/Medcare";
-import Questions from "../components/Faq/Questions";
-
-};
-
-export default Home;
