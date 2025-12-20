73a5d2ffcaa6a027003377fff76a7587ac2dc13d
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-19 22:00:28 -0500

    Update Doctors.jsx

--- front-sdt/src/pages/Doctors.jsx
+++ front-sdt/src/pages/Doctors.jsx
@@ -6,23 +6,7 @@ import Footer from "../components/Footer/Footer";
 
 const ArrowRightIcon = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-    <line x1="5" y1="12" x2="19" y2="12"></line>
-    <polyline points="12 5 19 12 12 19"></polyline>
-  </svg>
-);
 
-const DoctorList = () => {
-  const navigate = useNavigate();
-
-  const getDoctors = async () => {
-    try {
-      const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-      const res = await axios.get(`${apiUrl}/doctors`);
-      return res.data.doctors || res.data; 
-    } catch (error) {
-      toast.error("Error al conectar con el servidor");
-      throw error;
-    }
   };
 
   const { data: doctors = [], error, isLoading } = useQuery({
