4b5d66dd28e7d846954414c7f56649fa0b8d3a99
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-20 16:45:44 -0500

    Update tailwind.config.js

--- front-sdt/tailwind.config.js
+++ front-sdt/tailwind.config.js
@@ -10,11 +10,7 @@ export default {
         // Tu nueva paleta personalizada
         gainsboro: "#DCDCDC",
         gold: "#FFD700",
-        // Mantenemos los anteriores por si los usas en detalles técnicos
-        primaryColor: "#0067FF", // Corregido el hex que faltaba un dígito
         yellowColor: "#FEB60D",
-        purpleColor: "#9771FF",
-        IrisBlue: "#01B5C5",
         headingColor: "#000000", // Negro puro para encabezados
         textColor: "#000000",    // Negro puro para todo el texto
       },
