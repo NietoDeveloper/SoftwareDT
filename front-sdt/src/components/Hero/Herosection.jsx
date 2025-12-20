d93919a65cbb7b9376ad1829d0642310dc6d990e
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-20 16:39:06 -0500

    Update Herosection.jsx

--- front-sdt/src/components/Hero/Herosection.jsx
+++ front-sdt/src/components/Hero/Herosection.jsx
@@ -1,46 +1,4 @@
-import { Link } from "react-router-dom";
-import heroimage2 from "../../assets/images/banner5.png";
-import heroimage3 from "../../assets/images/hero-img03.png";
-import heroimage4 from "../../assets/images/feature-img.png";
 
-const Herosection = () => {
-  return (
-    <>
-
-      <section className="min-h-screen w-full flex flex-col justify-center items-center relative pt-16 pb-12 lg:pt-0 lg:pb-0">
-        <div className="container mx-auto px-6 flex flex-col lg:flex-row lg:items-center justify-between gap-10 lg:gap-20">
-          <div className="w-full lg:w-1/2 flex flex-col items-start z-10">
-            <h1
-              className="text-[36px] leading-[44px]
-                   text-gray-800 font-[600] lg:text-[56px] lg:leading-[64px] mb-6"
-            >
-              Construimos Software Que Da Solucion
-
-
-          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end z-10">
-            <div className="flex flex-col gap-5 max-w-[350px] lg:max-w-full">
-              <img
-                src={heroimage4}
-                alt="Diseño de Software"
-                className="rounded-xl shadow-xl w-full"
-              />
-              <div className="flex gap-5">
-                <img
-                  src={heroimage3}
-                  alt="Programación"
-                  className="rounded-xl shadow-xl w-1/2"
-                />
-                <img
-                  src={heroimage2}
-                  alt="Apps Móviles"
-                  className="rounded-xl shadow-xl w-1/2"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
   );
 };
 
