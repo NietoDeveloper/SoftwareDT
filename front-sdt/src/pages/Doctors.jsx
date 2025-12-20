154613c569d61c27abbcca6f7b4e45fe966f9cd2
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-19 22:00:35 -0500

    Update Doctors.jsx

--- front-sdt/src/pages/Doctors.jsx
+++ front-sdt/src/pages/Doctors.jsx
@@ -4,21 +4,6 @@ import { toast } from "react-toastify";
 import axios from "axios";
 import Footer from "../components/Footer/Footer";
 
-const ArrowRightIcon = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-
-  };
-
-  const { data: doctors = [], error, isLoading } = useQuery({
-    queryKey: ["doctors"],
-    queryFn: getDoctors,
-  });
-
-  const navigateToBooking = (doctor) => {
-    navigate(`/book-appointment/${doctor._id}`, { state: { doctorData: doctor } });
-  };
-
-
               </div>
             </div>
           ))}
