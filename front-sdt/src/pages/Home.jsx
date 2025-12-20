7ac7c969bd338b6dbd9f222c195da3e27959fcab
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-20 15:26:07 -0500

    Update Home.jsx

--- front-sdt/src/pages/Home.jsx
+++ front-sdt/src/pages/Home.jsx
@@ -3,21 +3,7 @@ import About from "../components/About";
 import Services from "../components/Offerings/Services";
 import Medcare from "../components/Preview/Medcare";
 import Questions from "../components/Faq/Questions";
-import Guide from "../components/Explainer/Guide";
-import BogotaAir1 from "../assets/images/BogotaAir1.mp4";
-import Footer from "../components/Footer/Footer";
 
-const Home = () => {
-  return (
-    <div className="bg-[#fcfcfc] antialiased">
-      {/* VIDEO SECTION - Manteniendo posición y forma */}
-        <video
-          autoPlay
- 
-        </div>
-
-    </div>
-  );
 };
 
 export default Home;
