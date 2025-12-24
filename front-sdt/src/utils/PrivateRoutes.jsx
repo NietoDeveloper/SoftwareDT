f3d54ac9041925b9d2658a6270666ae6390ca7b1
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-24 10:46:54 -0500

    Update PrivateRoutes.jsx

--- front-sdt/src/utils/PrivateRoutes.jsx
+++ front-sdt/src/utils/PrivateRoutes.jsx
@@ -3,18 +3,7 @@ import { Outlet, Navigate, useLocation } from "react-router-dom";
 import { UserContext } from "../context/UserContext"; 
 
 const PrivateRoutes = () => {
-    const { token, loading } = useContext(UserContext);
-    const location = useLocation(); 
-    
-    // Estado de carga con estilo Software DT (Sincronizado con tu paleta de colores)
-    if (loading) {
-        return (
-            <div className="min-h-screen flex flex-col items-center justify-center bg-[#DCDCDC]">
-                <div className="w-12 h-12 border-4 border-black border-t-[#FFD700] rounded-full animate-spin mb-4"></div>
-                <h1 className="text-[10px] font-black uppercase tracking-[0.4em] text-black animate-pulse">
-                    Verificando Credenciales...
-                </h1>
-            </div>
+
         
 
 }
