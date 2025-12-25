8b4e146351830040221ff975775bb6140972f2c6
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-24 22:29:42 -0500

    Update App.jsx

--- front-sdt/src/App.jsx
+++ front-sdt/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AppointmentConfirmation from "./pages/AppointmentConfirmation.jsx"; 
 import Header from "./components/Header/Header.jsx";
@@ -6,10 +7,7 @@ import "./App.css";
 import DoctorList from "./pages/Doctors.jsx";
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
-import Doctorsignup from "./pages/DoctorSignup.jsx";
-import Doctorlogin from "./pages/DoctorLogin.jsx";
 import Services from "./pages/Services.jsx";
-import { UserProvider } from "./context/UserContext.jsx"; 
 import PrivateRoutes from "./utils/PrivateRoutes.jsx";
 import BookingPage from "./pages/BookingPage.jsx";
 import Contact from "./pages/Contact.jsx";
@@ -17,13 +15,15 @@ import Payment from "./components/Checkout/Payment.jsx";
 import ClientPanel from "./pages/ClientAppointmentsPanel.jsx";
 import OurClients from "./pages/OurClients.jsx";
 
-// 1. IMPORTAR TOASTER
+// 1. IMPORTAR TOASTER (Mantenemos react-hot-toast para notificaciones de sistema)
 import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <UserProvider>
-      {/* 2. CONFIGURACIÓN GLOBAL DE NOTIFICACIONES ESTILO SDT */}
+    <>
+      {/* CONFIGURACIÓN GLOBAL DE NOTIFICACIONES ESTILO SOFTWARE DT 
+          Nota: Ya no envolvemos en UserProvider porque está en main.jsx
+      */}
       <Toaster 
         position="top-right"
         toastOptions={{
@@ -42,6 +42,7 @@ function App() {
       />
       
       <Header />
+      
       <Routes>
         {/* --- SECCIÓN PÚBLICA --- */}
         <Route path="/" element={<Home />} />
@@ -52,20 +53,23 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         
-        {/* --- SECCIÓN PROTEGIDA --- */}
+        {/* --- SECCIÓN PROTEGIDA (DATACENTER) --- */}
         <Route element={<PrivateRoutes />}>
+          {/* Mapeo de rutas al Panel de Cliente */}
           <Route path="/users/profile/me" element={<ClientPanel />} />
           <Route path="/client/dashboard" element={<ClientPanel />} />
           <Route path="/client-appointments" element={<ClientPanel />} />
           
+          {/* Flujo de Citas */}
           <Route path="/book-appointment/:doctorId" element={<BookingPage />} />
           <Route path="/checkout" element={<Payment />} />
           <Route path="/appointment-confirmation" element={<AppointmentConfirmation />} />
         </Route>
 
+        {/* Fallback de seguridad */}
         <Route path="*" element={<Home />} />
       </Routes>
-    </UserProvider>
+    </>
   );
 }
 
