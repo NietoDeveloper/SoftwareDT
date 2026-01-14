b1523ebbcaad0f7273afc4e30d9b83d4017964a8
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:55:30 -0500

    Update Herosection.jsx

--- front-sdt/src/components/Hero/Herosection.jsx
+++ front-sdt/src/components/Hero/Herosection.jsx
@@ -3,15 +3,7 @@ import heroimage2 from "../../assets/images/banner5.png";
 import heroimage3 from "../../assets/images/hero-img03.png";
 import heroimage4 from "../../assets/images/feature-img.png";
 
-const Herosection = () => {
-  return (
-    <section className="w-full bg-card py-12 lg:py-28 overflow-hidden">
 
-          
-        </div>
-      </div>
-    </section>
-  );
 };
 
 export default Herosection;
