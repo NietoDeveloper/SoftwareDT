a491a814ecc628bd60b6560255c4947671604668
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:52:18 -0500

    Update Header.jsx

--- front-sdt/src/components/Header/Header.jsx
+++ front-sdt/src/components/Header/Header.jsx
@@ -10,25 +10,7 @@ const MenuButton = ({ isOpen, onClick }) => {
   const variantCenter = { closed: { opacity: 1, x: 0 }, opened: { opacity: 0, x: -20 } };
   const variantBottom = { closed: { rotate: 0, y: 0 }, opened: { rotate: -45, y: -8 } };
 
-                </>
-              ) : (
-                <div className="w-full space-y-4 flex flex-col items-center">
-                  <Link 
-                    to="/users/profile/me" 
-                    onClick={() => setIsMenuOpen(false)} 
-                    className={`w-full py-3 rounded-xl border-2 border-[#FFD700] text-black text-xs font-black uppercase text-center ${
-                      isActive("/users/profile/me") ? "bg-[#FEB60D]" : "bg-[#FFD700]"
-                    }`}
-                  >
-                    Panel Activo
-                  </Link>
-                  
-                  <button 
-                    onClick={handleLogout}
-                    className="w-full py-2 text-red-600 font-black uppercase text-[10px] tracking-widest hover:text-red-700 transition-colors"
-                  >
-                    Salir
-                  </button>
+     
                 </div>
               )}
             </div>
