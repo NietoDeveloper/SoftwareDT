69b888c1a9db074221100f0696be37916a8b7204
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:55:24 -0500

    Update Herosection.jsx

--- front-sdt/src/components/Hero/Herosection.jsx
+++ front-sdt/src/components/Hero/Herosection.jsx
@@ -6,21 +6,7 @@ import heroimage4 from "../../assets/images/feature-img.png";
 const Herosection = () => {
   return (
     <section className="w-full bg-card py-12 lg:py-28 overflow-hidden">
-      <div className="max-w-[1800px] mx-auto px-4 md:px-8 lg:px-12">
-        <div className="flex flex-col lg:flex-row items-center justify-between gap-12 lg:gap-20">
-          
-          {/* LADO IZQUIERDO: TEXTO DE ALTO IMPACTO */}
-          <div className="w-full lg:w-[65%] flex flex-col items-start z-10">
 
-
-
-
-             
-                  className="rounded-[2.5rem] shadow-2xl w-1/2 border-[8px] border-main transition-all duration-700 hover:rotate-3 hover:scale-105"
-                />
-              </div>
-            </div>
-          </div>
           
         </div>
       </div>
