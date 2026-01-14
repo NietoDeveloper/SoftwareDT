c8cf9e74ca704dc424527535a96aa35cf7bd3696
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:59:59 -0500

    Update Services.jsx

--- front-sdt/src/components/Offerings/Services.jsx
+++ front-sdt/src/components/Offerings/Services.jsx
@@ -3,7 +3,7 @@ import ServicesCard from "./ServicesCard";
 const servicesList = [
   {
     name: "Desarrollo de Web, Apps y Bases de Datos",
-    dos, adaptados a tus SEO integrado. Desarrollo de Apps: Aplicaciones móviles personalizadas para iOS y Android, con interfaces intuitivas y funcionalidades específicas para tu negocio. Bases de Datos: Diseño y gestión de bases de datos seguras, escalables y eficientes, optimizadas para almacenar, consultar y analizar información.",
+    licaciones móviles personalizadas para iOS y Android, con interfaces intuitivas y funcionalidades específicas para tu negocio. Bases de Datos: Diseño y gestión de bases de datos seguras, escalables y eficientes, optimizadas para almacenar, consultar y analizar información.",
   },
 
 
