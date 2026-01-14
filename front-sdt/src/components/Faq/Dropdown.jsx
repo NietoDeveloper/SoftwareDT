ca9687b1364c38d43d197a10536ba0a728194fbd
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:20:24 -0500

    Update Dropdown.jsx

--- front-sdt/src/components/Faq/Dropdown.jsx
+++ front-sdt/src/components/Faq/Dropdown.jsx
@@ -1,13 +1,4 @@
 import { useState } from 'react';
 import { BsCaretDown, BsCaretUp } from 'react-icons/bs';
 
-const FaqItem = ({ question, answer }) => {
-  const [open, setOpen] = useState(false); // Maintain state for each FAQ item
-
-
-
-
-  );
-};
-
 export default FaqItem;
