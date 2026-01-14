ee3295e72bd01279664c828ee88d0ed1c294a492
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 22:37:54 -0500

    Update api.js

--- front-sdt/src/API/api.js
+++ front-sdt/src/API/api.js
@@ -14,28 +14,6 @@
 
            
                 
-              
-
-                originalRequest._retry = true;
-                isRefreshing = true;
-
-                return new Promise(async (resolve, reject) => {
-                    try {
-                        oken', cleanNewToken); // Actualiza Disco
-                        
-                        originalRequest.headers.Authorization = `Bearer ${cleanNewToken}`;
-                        processQueue(null, cleanNewToken);
-                        resolve(axiosPrivate(originalRequest));
-                    } catch (refreshError) {
-                        processQueue(refreshError, null);
-                        console.error("🚨 SDT: Sesión caducada definitivamente.");
-                        if (onLogout) onLogout();
-                        reject(refreshError);
-                    } finally {
-                        isRefreshing = false;
-                    }
-                });
-            }
 
             return Promise.reject(error);
         }
