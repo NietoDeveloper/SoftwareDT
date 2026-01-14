85c3bdda514ddbc72ba4547826e8acd5f44f9d95
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:12:43 -0500

    Update Guide.jsx

--- front-sdt/src/components/Explainer/Guide.jsx
+++ front-sdt/src/components/Explainer/Guide.jsx
@@ -3,15 +3,7 @@ import equipo1 from '../../assets/images/equipo1.png';
 
 const Guide = () => {
   return (
-    <>
-      <section className="bg-card py-20">
-        <div className="container mx-auto px-6">
-       
 
-
-           
-
-          </div>
         </div>
       </section>
     </>
