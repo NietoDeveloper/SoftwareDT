0cf70cca06cbad899f98f792e078ac448388c3c0
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:33:20 -0500

    Update Questions.jsx

--- front-sdt/src/components/Faq/Questions.jsx
+++ front-sdt/src/components/Faq/Questions.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import FaqItem from "./Dropdown";
-import equipo2 from '../../assets/images/equipo2.png';
-import softlive from '../../assets/images/softlive.mp4';
-
-const Questions = () => {
-  const sdtGold = "#FFD700";
-
-  return (
 
         </div>
       </div>
