7b0365aca740375fb308d1083ea7be8417af2379
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:47:24 -0500

    Update Footer.jsx

--- front-sdt/src/components/Footer/Footer.jsx
+++ front-sdt/src/components/Footer/Footer.jsx
@@ -12,11 +12,7 @@ const Footer = () => {
       
             </p>
 
-            <div className="flex items-center gap-4 py-2 cursor-default"
-                 onMouseEnter={() => handleMouseEnter('date')} onMouseLeave={handleMouseLeave}>
-              <span className="w-3 h-3 bg-[#FFD700] rounded-full animate-pulse shadow-[0_0_10px_#FFD700]"></span>
-              <p style={getStyle('date')} className="transition-colors duration-300 text-lg sm:text-xl font-black uppercase tracking-tighter">
-                {currentDateTime} <span style={{opacity: 0.5, color: '#FFF'}}>|</span> BOGOTÁ, COLOMBIA
+          Time} <span style={{opacity: 0.5, color: '#FFF'}}>|</span> BOGOTÁ, COLOMBIA
               </p>
             </div>
           </div>
