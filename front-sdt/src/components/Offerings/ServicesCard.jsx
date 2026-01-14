c6e91fae8c8a514e2af7d8fef6b790df77d4799e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:57:27 -0500

    Update ServicesCard.jsx

--- front-sdt/src/components/Offerings/ServicesCard.jsx
+++ front-sdt/src/components/Offerings/ServicesCard.jsx
@@ -3,10 +3,3 @@ import { BsArrowRight } from 'react-icons/bs';
 
 const ServicesCard = ({ item, index }) => {
   const { name, desc, _id } = item; 
-  
-  return (
-    <div className="py-[30px] px-3 lg:px-5 bg-white rounded-3xl border-2 border-transparent hover:border-gold transition-all duration-300 group/card">
-
-};
-
-export default ServicesCard;
