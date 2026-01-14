e4ab442dd94ba4d7ac590f2dc8fda06276999a95
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 22:53:58 -0500

    Update Publicidad.jsx

--- front-sdt/src/components/Publicidad.jsx
+++
@@ -1,6 +0,0 @@
-import techs1 from "../assets/images/techs1.png";
-import techs2 from "../assets/images/techs2.png";
-import feature from "../assets/images/softwareempresas.mp4";
-import { Link } from "react-router-dom";
-import { BsArrowRight } from "react-icons/bs";
-
