bc9b89b71ed5da1af1f27c678df0a0d002727222
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:55:35 -0500

    Update Herosection.jsx

--- front-sdt/src/components/Hero/Herosection.jsx
+++ front-sdt/src/components/Hero/Herosection.jsx
@@ -1,9 +1,3 @@
-import { Link } from "react-router-dom";
-import heroimage2 from "../../assets/images/banner5.png";
-import heroimage3 from "../../assets/images/hero-img03.png";
-import heroimage4 from "../../assets/images/feature-img.png";
 
-
-};
 
 export default Herosection;
