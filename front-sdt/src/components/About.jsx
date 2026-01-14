13c8613b4716f4ea4b190cc804e64493fd1c2e7e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:05:55 -0500

    Update About.jsx

--- front-sdt/src/components/About.jsx
+++ front-sdt/src/components/About.jsx
@@ -3,17 +3,7 @@ import { Link } from "react-router-dom";
 
 const About = () => {
 
-              {/* BOTÓN ESTILO SOFTWARE DT (AJUSTADO) */}
-              <button 
-                className="mt-10 w-56 h-14 bg-black text-white text-xl font-black uppercase tracking-tighter 
-                           rounded-full transition-all duration-400 shadow-2xl active:scale-95
-                           hover:bg-[#FEB60D] hover:text-black hover:-translate-y-1.5 
-                           hover:shadow-[0_0_30px_rgba(254,182,13,0.8)]"
-              >
-                Nuestros Clientes
-              </button>
-            </Link>
-          </div>
+
         </div>
       </div>
     </section>
