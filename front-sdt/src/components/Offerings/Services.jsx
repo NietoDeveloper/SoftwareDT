252af5dcf4462b181dbee0f1ef9ae3e5d21fa444
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 00:00:25 -0500

    Update Services.jsx

--- front-sdt/src/components/Offerings/Services.jsx
+++ front-sdt/src/components/Offerings/Services.jsx
@@ -2,8 +2,7 @@ import ServicesCard from "./ServicesCard";
 
 const servicesList = [
   {
-    name: de Datos: Diseño y gestión de bases de datos seguras, escalables y eficientes, optimizadas para almacenar, consultar y analizar información.",
-  },
+    name: de Dato bases de dato  },
 
 
 
