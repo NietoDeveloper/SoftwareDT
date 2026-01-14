0ac9662d4c6490d7c6eddd28e7d07b6a86e7e784
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:58:40 -0500

    Update Services.jsx

--- front-sdt/src/components/Offerings/Services.jsx
+++ front-sdt/src/components/Offerings/Services.jsx
@@ -6,14 +6,6 @@ const servicesList = [
     desc: "Creación de sitios web dinámicos, responsivos y optimizados, adaptados a tus necesidades, con diseño moderno y SEO integrado. Desarrollo de Apps: Aplicaciones móviles personalizadas para iOS y Android, con interfaces intuitivas y funcionalidades específicas para tu negocio. Bases de Datos: Diseño y gestión de bases de datos seguras, escalables y eficientes, optimizadas para almacenar, consultar y analizar información.",
   },
 
-  {
-    name: "Soluciones para Empresas",
-    desc: "Software Contable: Automatiza registros financieros, genera balances, estados de resultados y cumple con normativas fiscales. Simplifica la contabilidad con herramientas para facturación, conciliación bancaria y reportes financieros. Manejo de Personal: Administra nóminas, calcula salarios, beneficios y deducciones. Controla horarios, asistencias y expedientes de empleados, optimizando la gestión de recursos humanos. Manejo de Usuarios e Información: Gestiona accesos y permisos de usuarios con seguridad. Organiza y protege datos empresariales, facilitando la búsqueda, almacenamiento y análisis de información clave.",
-  },
-
-
-
-];
 
 const Services = () => {
   return (
