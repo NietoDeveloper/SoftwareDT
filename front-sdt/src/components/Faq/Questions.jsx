a0714b57e5ee8b02f0e6bcb69092f3ad8750c3aa
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:33:08 -0500

    Update Questions.jsx

--- front-sdt/src/components/Faq/Questions.jsx
+++ front-sdt/src/components/Faq/Questions.jsx
@@ -13,18 +13,7 @@ const Questions = () => {
                 answer="Virtual. La reunión se llevará a cabo vía Microsoft TEAMS y tiene una duración estimada de 15 minutos."
               />
               <FaqItem
-                question="¿Tienen Atención en todo el país?"
-                answer="Sí. Antes de iniciar la construcción del software se agendan reuniones estratégicas. De ser necesario, se realizarán de forma presencial en la ciudad del cliente."
-              />
-            </div>
-            
-            <div className="mt-16 flex flex-col items-center lg:items-start">
-               <p className="text-[14px] font-black tracking-[0.6em] uppercase" style={{color: sdtGold}}>
-                 Dorado Technologies
-               </p>
-               <div className="w-16 h-[2px] bg-white/50 mt-2"></div>
-            </div>
-          </div>
+       
         </div>
       </div>
     </section>
