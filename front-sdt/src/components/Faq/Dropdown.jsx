2f97148d1e47ed4e8a898d080c024f79cc44f05c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:20:15 -0500

    Update Dropdown.jsx

--- front-sdt/src/components/Faq/Dropdown.jsx
+++ front-sdt/src/components/Faq/Dropdown.jsx
@@ -4,9 +4,6 @@ import { BsCaretDown, BsCaretUp } from 'react-icons/bs';
 const FaqItem = ({ question, answer }) => {
   const [open, setOpen] = useState(false); // Maintain state for each FAQ item
 
-  const handleFaq = () => {
-    setOpen(!open); // Toggle the state for the specific FAQ item
-  };
 
 
 
