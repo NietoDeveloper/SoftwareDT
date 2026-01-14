22e536859fd247769830523ab949b31fe7d3d946
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 00:00:11 -0500

    Update Services.jsx

--- front-sdt/src/components/Offerings/Services.jsx
+++ front-sdt/src/components/Offerings/Services.jsx
@@ -3,7 +3,7 @@ import ServicesCard from "./ServicesCard";
 const servicesList = [
   {
     name: "Desarrollo de Web, Apps y Bases de Datos",
-on interfaces intuitivas y funcionalidades específicas para tu negocio. Bases de Datos: Diseño y gestión de bases de datos seguras, escalables y eficientes, optimizadas para almacenar, consultar y analizar información.",
+on interfaces intuu negocio. Bases de Datos: Diseño y gestión de bases de datos seguras, escalables y eficientes, optimizadas para almacenar, consultar y analizar información.",
   },
 
 
