56418c16feca92dc4e52615bfb49ff498bf0c8aa
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:05:39 -0500

    Update About.jsx

--- front-sdt/src/components/About.jsx
+++ front-sdt/src/components/About.jsx
@@ -8,14 +8,7 @@ const About = () => {
         <div
           className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col
             lg:flex-row items-center"
-        >
-          {/* LADO DE LA IMAGEN - Nieto Developer Style */}
-          <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
-            <img 
-              src={about1} 
-              alt="Sobre Software DT" 
-              className="rounded-[2rem] border-8 border-main shadow-2xl" 
-            />
+
             
 
 
