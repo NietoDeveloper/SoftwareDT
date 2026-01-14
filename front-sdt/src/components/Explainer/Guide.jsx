4e26e67b543877d9e5d6571aa0d62e14671bdc8a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:11:59 -0500

    Update Guide.jsx

--- front-sdt/src/components/Explainer/Guide.jsx
+++ front-sdt/src/components/Explainer/Guide.jsx
@@ -23,14 +23,7 @@ const Guide = () => {
 
               <Link to="/services">
                 {/* BOTÓN ESTILO SOFTWARE DT (ESTÁNDAR UNIFICADO) */}
-                <button 
-                  className="w-56 h-14 bg-black text-white text-xl font-black uppercase tracking-tighter 
-                             rounded-full transition-all duration-400 shadow-2xl active:scale-95
-                             hover:bg-[#FEB60D] hover:text-black hover:-translate-y-1.5 
-                             hover:shadow-[0_0_30px_rgba(254,182,13,0.8)]"
-                >
-                  Iniciar Proceso
-                </button>
+   
               </Link>
             </div>
 
