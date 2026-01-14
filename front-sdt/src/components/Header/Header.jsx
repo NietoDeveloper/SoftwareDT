d27d2483f5d6263f4644e90d66aa7cba7d9bfb8e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:52:30 -0500

    Update Header.jsx

--- front-sdt/src/components/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import { useState, useContext } from "react"; 
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import { motion, AnimatePresence } from "framer-motion";
-// CORRECCIÓN DE RUTA: Subimos dos niveles para llegar a src/context
-import { UserContext } from "../../context/UserContext"; 
-
-/
