dff067cb88299289f8c774f031f77f6e303a58a9
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-13 23:08:42 -0500

    Update Payment.jsx

--- front-sdt/src/components/Checkout/Payment.jsx
+++ front-sdt/src/components/Checkout/Payment.jsx
@@ -8,11 +8,6 @@ const Payment = () => {
             <form>
                 <input type="email" required/>
 
-                <div>
-                
-                 
-
-                </div>
 
               
 
