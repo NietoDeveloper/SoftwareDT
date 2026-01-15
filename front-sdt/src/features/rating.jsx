8c03707d89d313847e7a62b90a31233ead5a0d60
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 22:34:11 -0500

    Update rating.jsx

--- front-sdt/src/features/rating.jsx
+++ front-sdt/src/features/rating.jsx
@@ -4,10 +4,6 @@ const ReviewForm = () => {
   const [rating, setRating] = useState(0);
   const [reviewText, setReviewText] = useState('');
 
-  const handlestarClick = (selectedRating) => {
-    setRating(selectedRating);
-  };
-
 
 
 export default ReviewForm;
