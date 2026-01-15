93de48edb21cb5f1ef59bac0012b404d076d0733
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 22:34:17 -0500

    Update rating.jsx

--- front-sdt/src/features/rating.jsx
+++ front-sdt/src/features/rating.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 
 const ReviewForm = () => {
-  const [rating, setRating] = useState(0);
-  const [reviewText, setReviewText] = useState('');
-
 
 
 export default ReviewForm;
