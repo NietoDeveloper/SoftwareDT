3d21e21d2cc3a25280e8d6efb0eda1e06355afab
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 00:26:41 -0500

    Update AppointmentConfirmation.jsx

--- front-sdt/src/pages/AppointmentConfirmation.jsx
+++ front-sdt/src/pages/AppointmentConfirmation.jsx
@@ -2,16 +2,6 @@
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import { useEffect, useContext } from "react"; // Añadido useContext
 import { UserContext } from "../context/UserContext.jsx"; // Importamos el contexto
-import {
-  Calendar,
-  C rounded-2xl bg-gold text-black transition-all group-hover:rotate-12 flex-shrink-0">
-               <ShieldCheck size={24} className="sm:w-8 sm:h-8" />
-             </div>
-             <div className="ml-5 min-w-0">
-                <p className="text-[9px] font-black text-gold uppercase tracking-widest">Protocolo Activo</p>
-                <h2 className="text-lg sm:text-xl font-black uppercase truncate">{displayData.serviceName}</h2>
-v>
-  </div>
-);
+impo
 
 export default AppointmentConfirmation;
