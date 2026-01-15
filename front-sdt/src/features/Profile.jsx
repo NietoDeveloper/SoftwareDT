67d5b8fe59c9516fee94f320e7f17562a947e549
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 23:17:17 -0500

    Update Profile.jsx

--- front-sdt/src/features/Profile.jsx
+++ front-sdt/src/features/Profile.jsx
@@ -32,12 +32,7 @@ const Profile = () => {
                     </Link>
                 </li>
                 <hr className="my-1 border-gray-200" />
-                <li 
-                    className="p-2 hover:bg-red-50 rounded-md cursor-pointer transition duration-150 text-red-600 font-medium" 
-                    onClick={logoutFromApiAndState}
-                >
-                    🚪 Cerrar Sesión
-                </li>
+            
             </ul>
         </div>
     )
