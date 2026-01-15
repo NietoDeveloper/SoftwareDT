5b8ab84d37d830edf2fefdbca83e4c2802dfab21
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 22:08:15 -0500

    Update UserContext.jsx

--- front-sdt/src/context/UserContext.jsx
+++ front-sdt/src/context/UserContext.jsx
@@ -65,5 +65,5 @@ export const UserProvider = ({ children }) => {
 export const useUser = () => {
     const context = useContext(UserContext);
     if (!context) throw new Error("useUser debe ser usado dentro de un UserProvider");
-    return context;
+
 };
