8c9d04c07006f2be923d072c2e8c51a835d97d8d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 22:09:13 -0500

    Update UserContext.jsx

--- front-sdt/src/context/UserContext.jsx
+++ front-sdt/src/context/UserContext.jsx
@@ -2,18 +2,3 @@ import { createContext, useState, useEffect, useCallback, useRef, useMemo, useCo
 import { setupInterceptors } from "../API/api.js";
 
 export const UserContext = createContext(null);
-
-export const UserProvider = ({ children }) => {
-    // 1. Estado de Usuario con inicialización segura
-    const [user, setUser] = useState(() => {
-        try {
-            const savedUser = localStorage.getItem('user');
-            if (!savedUser || savedUser === "undefined" || savedUser === "null") return null;
-            return JSON.parse(savedUser);
-        } catch (e) {
-            return null;
-        }
-    });
-
-
-
