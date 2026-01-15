693b6f6122245fe45761dc1a8220e53436766ba3
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 23:18:39 -0500

    Update Profile.jsx

--- front-sdt/src/features/Profile.jsx
+++ front-sdt/src/features/Profile.jsx
@@ -1,7 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { axiosAuth } from '../API/api';
 import { UserContext } from '../context/UserContext'; 
-import { useContext } from 'react';
 
 
 
