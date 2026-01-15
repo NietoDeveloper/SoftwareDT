3df61e446073d3d0260b9270ac2deb2166323263
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 00:26:47 -0500

    Update AppointmentConfirmation.jsx

--- front-sdt/src/pages/AppointmentConfirmation.jsx
+++ front-sdt/src/pages/AppointmentConfirmation.jsx
@@ -1,7 +1,2 @@
 /* eslint-disable react/no-unescaped-entities */
 import { useLocation, Link, useNavigate } from "react-router-dom";
-import { useEffect, useContext } from "react"; // Añadido useContext
-import { UserContext } from "../context/UserContext.jsx"; // Importamos el contexto
-impo
-
-export default AppointmentConfirmation;
