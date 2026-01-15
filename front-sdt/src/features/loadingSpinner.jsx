27ef44087508be3dac2906cce3757759f2c24ef7
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 22:11:51 -0500

    Update loadingSpinner.jsx

--- front-sdt/src/features/loadingSpinner.jsx
+++ front-sdt/src/features/loadingSpinner.jsx
@@ -3,13 +3,7 @@ const Spinner = () => {
     <div className="flex items-center justify-center min-h-[50px]">
       
       <div 
-        className="
-          w-12 h-12 
-          border-4 border-solid 
-          rounded-full 
-          border-gray-200 
-          border-t-indigo-500 
-          animate-spin
+
         "
       >
       </div>
