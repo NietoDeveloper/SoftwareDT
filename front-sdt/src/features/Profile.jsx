8c15db468de49c101040b14724b9d56e581904ec
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 22:38:35 -0500

    Update Profile.jsx

--- front-sdt/src/features/Profile.jsx
+++ front-sdt/src/features/Profile.jsx
@@ -22,7 +22,7 @@ const Profile = () => {
     return (
         <div className="absolute bg-white shadow-lg rounded-lg p-2 w-48 top-20 right-5 z-10 border border-gray-100">
             <ul className="flex flex-col text-sm">
-                <li className="p-2 hover:bg-gray-100 rounded-md cursor-pointer transition duration-150">
+                <li className="p-2 hover:bg-gsor-pointer transition duration-150">
                     <Link to="/user/profile" className="w-full block text-gray-700 font-medium">
                         👤 Mi Perfil
                     </Link>
