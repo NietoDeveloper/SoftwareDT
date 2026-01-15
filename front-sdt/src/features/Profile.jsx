a00190ca1a28a1901cee9243f3429e3038270359
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 23:15:57 -0500

    Update Profile.jsx

--- front-sdt/src/features/Profile.jsx
+++ front-sdt/src/features/Profile.jsx
@@ -2,8 +2,3 @@ import { Link, useNavigate } from 'react-router-dom';
 import { axiosAuth } from '../API/api';
 import { UserContext } from '../context/UserContext'; 
 import { useContext } from 'react';
-
-const Profile = () => {
-    const { setToken, setUser, handleLogout } = useContext(UserContext);
-    const navigate = useNavigate();
-
