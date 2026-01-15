f55338e7e16c96d51f819eb3f598bda7c88b0cfc
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 22:32:50 -0500

    Update rating.jsx

--- front-sdt/src/features/rating.jsx
+++ front-sdt/src/features/rating.jsx
@@ -13,15 +13,11 @@ const ReviewForm = () => {
       <div className='container'>
         
         <form >             
-  
-  
-            
-           
             
             <div className=''>
                  <button
               type="submit"
-              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md transition-colors"
+             
             >
               Submit Review
             </button>
