76189fac445ad93469e4df32501d01fec3352845
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 23:21:19 -0500

    Update useRefresh.jsx

--- front-sdt/src/hooks/useRefresh.jsx
+++ front-sdt/src/hooks/useRefresh.jsx
@@ -1,12 +1,2 @@
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext"; 
-import refreshAccessToken from "../utils/refreshAccess";
-
-const useRefresh = () => {
-  // Extraemos setToken y setUser para limpiar la sesión completa si el refresco falla
-  const { setToken, setUser } = useContext(UserContext);
-
-
-s
-
-export default useRefresh;
