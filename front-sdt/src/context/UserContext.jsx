63246441d5115defd6defe9f9f80aad39447e102
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-14 22:09:19 -0500

    Update UserContext.jsx

--- front-sdt/src/context/UserContext.jsx
+++ front-sdt/src/context/UserContext.jsx
@@ -1,4 +1,1 @@
 import { createContext, useState, useEffect, useCallback, useRef, useMemo, useContext } from "react";
-import { setupInterceptors } from "../API/api.js";
-
-export const UserContext = createContext(null);
