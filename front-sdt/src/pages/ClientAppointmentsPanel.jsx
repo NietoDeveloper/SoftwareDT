c758831dc946281f874be6b6bb12e9cd0a288eff
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 18:59:40 -0500

    Update ClientAppointmentsPanel.jsx

--- front-sdt/src/pages/ClientAppointmentsPanel.jsx
+++ front-sdt/src/pages/ClientAppointmentsPanel.jsx
@@ -1,17 +1,5 @@
 import { useState, useEffect, useContext, useCallback } from "react";
-import { UserContext } from "../context/UserContext.jsx";
-import { Link, useNavigate } from "react-router-dom";
-import { axiosPrivate } from "../API/api.js";
-import { 
-  PlusCircle, Mail, MessageCircle, ArrowUpRight, Loader2, Save
-} from "lucide-react";
-import { toast } from "react-hot-toast"; // Asumiendo que usas toast para feedback profesional
-
-          </div>
-        </aside>
-      </div>
-    </div>
-  );
+import { Use
 };
 
 export default ClientAppointmentsPanel;
