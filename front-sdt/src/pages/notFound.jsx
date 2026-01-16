e95219518d33b86c071ce0b0903e98c27dd4eb3f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 21:10:56 -0500

    Update notFound.jsx

--- front-sdt/src/pages/notFound.jsx
+++ front-sdt/src/pages/notFound.jsx
@@ -1,12 +1,2 @@
-import { useNavigate } from "react-router-dom";
-import BogotaAir1 from "../assets/images/BogotaAir1.mp4";
-
-const NotFoundPage = () => {
-  const navigate = useNavigate();
-
-  return (
-    <div className="bg-main antialiased min-h-screen relative flex items-center justify-center overflow-hidden">
-
-
 
 export default NotFoundPage;
