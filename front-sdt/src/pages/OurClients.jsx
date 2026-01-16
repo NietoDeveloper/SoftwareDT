0feadb5cb28f53c8a4830e9e679eaf7b20104720
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 21:26:22 -0500

    Update OurClients.jsx

--- front-sdt/src/pages/OurClients.jsx
+++ front-sdt/src/pages/OurClients.jsx
@@ -1,20 +1,3 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
-import Footer from "../components/Footer/Footer";
 
-const clientData = [
-
-
-
-];
-
-const OurClients = () => {
-  return (
-    <div className="bg-[#fcfcfc] min-h-screen font-sans antialiased">
-      <section className="flex flex-col items-center py-16 px-4 sm:px-8 max-w-[1800px] mx-auto">
-        
-
-        {/* Grid Responsivo (320px a 1800px) */}
-        <div className="w-full">
 
 export default OurClients;
