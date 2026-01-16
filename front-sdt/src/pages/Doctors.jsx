38d11968f33ab21940ccb0b7520173e5ceeae512
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 19:49:47 -0500

    Update Doctors.jsx

--- front-sdt/src/pages/Doctors.jsx
+++ front-sdt/src/pages/Doctors.jsx
@@ -18,18 +18,6 @@ iceData: selectedServiceInfo
     <div className="min-h-screen bg-main font-sans antialiased overflow-x-hidden">
       <div className="mx-auto px-4 sm:px-6 py-12 sm:py-20 max-w-[1800px]">
 
-
-      -y-2 cursor-pointer"
-            >
-              {/* STATUS INDICATOR */}
-              <div className="absolute top-6 right-6">
-                <div className="w-2 h-2 bg-gold rounded-full shadow-[0_0_8px_rgba(255,215,0,0.6)]"></div>
-              </div>
-
-          ))}
-        </div>
-      </div>
-      <Footer />
     </div>
   );
 };
