f7666dc6ab61530123f01c277c3d6cde3ddb59f2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 20:56:36 -0500

    Update Home.jsx

--- front-sdt/src/pages/Home.jsx
+++
@@ -1,5 +0,0 @@
-import { useNavigate } from "react-router-dom";
-import Herosection from "../components/Hero/Herosection";
-import About from "../components/About";
-import Publicidad from "../components/Publicidad";
-import Questions from "../components/Faq/Questions";
