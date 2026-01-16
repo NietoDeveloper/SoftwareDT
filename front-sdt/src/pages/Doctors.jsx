350842af2f34f65bdf6c20ff233fbc16fc70bc2a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 19:49:52 -0500

    Update Doctors.jsx

--- front-sdt/src/pages/Doctors.jsx
+++ front-sdt/src/pages/Doctors.jsx
@@ -1,25 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
-import { useNavigate, useLocation } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { toast } from "react-toastify";
-import axios from "axios";
-import { motion } from "framer-motion";
-import Footer from "../components/Footer/Footer";
-import { ArrowRight } from "lucide-react";
 
-const DoctorList = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  
-iceData: selectedServiceInfo
-
-
-  return (
-    <div className="min-h-screen bg-main font-sans antialiased overflow-x-hidden">
-      <div className="mx-auto px-4 sm:px-6 py-12 sm:py-20 max-w-[1800px]">
-
-    </div>
-  );
 };
 
 export default DoctorList;
