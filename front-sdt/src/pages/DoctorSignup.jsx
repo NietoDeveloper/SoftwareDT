28ea224d4fc8a6943c1fdb23cad29c6d4bb36f61
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 20:51:05 -0500

    Update DoctorSignup.jsx

--- front-sdt/src/pages/DoctorSignup.jsx
+++ front-sdt/src/pages/DoctorSignup.jsx
@@ -6,17 +6,7 @@ import { toast } from 'react-toastify';
 
 const RegisterIcon = (props) => (
     <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        className="h-6 w-6" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
-        {...props}
-        aria-hidden="true"
-    >
+
         <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
         <polyline points="14 2 14 8 20 8"></polyline>
         <line x1="16" y1="13" x2="8" y2="13"></line>
