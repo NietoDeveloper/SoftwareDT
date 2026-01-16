62957c724ff8ea25993716ffcd6590603d3ef063
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 19:47:02 -0500

    Update DoctorLogin.jsx

--- front-sdt/src/pages/DoctorLogin.jsx
+++ front-sdt/src/pages/DoctorLogin.jsx
@@ -1,23 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
-// import { AppContext } from '../context/UserContext'; 
 
-const DoctorIcon = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-        <path d="M12 2a3 3 0 0 0-3 3v2a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3z"></path>
-        <path d="M10 18h4"></path>
-        <path d="M12 22a2 2 0 0 0 2-2v-3h-4v3a2 2 0 0 0 2 2z"></path>
-        <path d="M8 15v-1a4 4 0 0 1 8 0v1"></path>
-    </svg>
-);
-
-const Doctorlogin = () => {
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const navigate = useNavigate();
-    
-   
     };
 
             </div>
