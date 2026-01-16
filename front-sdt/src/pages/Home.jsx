7a751bc30f8681ee3eef57158b48c8b821934671
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-15 20:56:31 -0500

    Update Home.jsx

--- front-sdt/src/pages/Home.jsx
+++ front-sdt/src/pages/Home.jsx
@@ -3,32 +3,3 @@ import Herosection from "../components/Hero/Herosection";
 import About from "../components/About";
 import Publicidad from "../components/Publicidad";
 import Questions from "../components/Faq/Questions";
-import Guide from "../components/Explainer/Guide";
-import BogotaAir1 from "../assets/images/BogotaAir1.mp4";
-import Footer from "../components/Footer/Footer";
-
-const Home = () => {
-  const navigate = useNavigate();
-
-  return (
-
-
-        .text-white-force {
-          color: #ffffff !important;
-          letter-spacing: 0.4em !important;
-          filter: drop-shadow(0 0 15px rgba(255, 215, 0, 0.9));
-        }
-
-
-
-        html { scroll-behavior: smooth; }
-      `,
-        }}
-      />
-
-   
-
-  );
-};
-
-export default Home;
