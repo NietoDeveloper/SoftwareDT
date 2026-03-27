e5b8a2e6941dd00cab4216037c23956ec60d5559
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-27 00:20:51 -0500

    Update userController.js

--- back-sdt/controllers/userController.js
+++ back-sdt/controllers/userController.js
@@ -2,9 +2,3 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/User'); 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
-// --- 1. Registro de Usuario (Con Auto-Login de Clase Mundial) ---
-const userRegister = asyncHandler(async (req, res) => {
-
-
-// --- 3. Actualizar Detalles (Update) ---
