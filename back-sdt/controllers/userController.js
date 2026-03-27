ac970beee7f5a96f9699eb612d838e34fa6177ea
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-27 00:20:45 -0500

    Update userController.js

--- back-sdt/controllers/userController.js
+++ back-sdt/controllers/userController.js
@@ -8,12 +8,3 @@ const userRegister = asyncHandler(async (req, res) => {
 
 
 // --- 3. Actualizar Detalles (Update) ---
-const updateUserDetails = asyncHandler(async (req, res) => {
-    const id = req.params.id || req.id; 
-
-
-    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-    res.sendStatus(204);
-});
-
-module.exports = { userRegister, userLogin, updateUserDetails, handleUserLogout };
