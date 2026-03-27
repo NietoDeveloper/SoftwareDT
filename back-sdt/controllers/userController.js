3fc88b147845c8a132a97ad75aa808545c834546
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-27 00:20:38 -0500

    Update userController.js

--- back-sdt/controllers/userController.js
+++ back-sdt/controllers/userController.js
@@ -10,16 +10,7 @@ const userRegister = asyncHandler(async (req, res) => {
 // --- 3. Actualizar Detalles (Update) ---
 const updateUserDetails = asyncHandler(async (req, res) => {
     const id = req.params.id || req.id; 
-    const { name, email, password, phone, photo, customMessage } = req.body;
-f (customMessage !== undefined) foundUser.customMessage = customMessage;
 
-    const updated = await foundUser.save();
-s
-
-    if (foundUser) {
-        foundUser.refreshToken = foundUser.refreshToken.filter(token => token !== refreshToken);
-        await foundUser.save();
-    }
 
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
     res.sendStatus(204);
