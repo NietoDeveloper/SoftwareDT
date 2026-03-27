329636ff8f1c0390a2d0ac0f1a8e12cb3bc104c0
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-26 23:56:04 -0500

    Update optionalAccess.js

--- back-sdt/middleware/optionalAccess.js
+++ back-sdt/middleware/optionalAccess.js
@@ -4,16 +4,4 @@ const optionalAccess = (req, res, next) => {
 
 
     const token = authHeader.split(' ')[1];
-
-    // 2. Verificación silenciosa del token
-    jwt..roles = Array.isArray(userInfo.roles) ? userInfo.roles : [];
-            }
-            
-            // Siempre continuamos el flujo
-            next();
-        }
-    );
-};
-
-// Exportación directa para que app.use(optionalAccess) funcione en server.js
-module.exports = optionalAccess;
+xports = optionalAccess;
