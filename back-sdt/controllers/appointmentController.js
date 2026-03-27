ef3c3012ce055a4994ffee0154015c3fcaf087ee
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-27 00:00:12 -0500

    Update appointmentController.js

--- back-sdt/controllers/appointmentController.js
+++
@@ -1,7 +0,0 @@
-const Appointment = require('../models/Appointment');
-const User = require('../models/User');
-const Doctor = require('../models/Doctor');
-const asyncHandler = require('express-async-handler');
-
-        return res.status(400).json({ success: false, message: "Identificador de usuario no detectado." });
-
