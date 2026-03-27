9d7f0329770fdefe571c8a680a7dc7e651cf05bf
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-03-27 00:00:06 -0500

    Update appointmentController.js

--- back-sdt/controllers/appointmentController.js
+++ back-sdt/controllers/appointmentController.js
@@ -1,4 +1,3 @@
-// back-sdt/controllers/appointmentController.js
 const Appointment = require('../models/Appointment');
 const User = require('../models/User');
 const Doctor = require('../models/Doctor');
