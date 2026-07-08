8c7cef9561a1c1166be3f30db13edc7224a36f49
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-07-07 23:53:31 -0500

    Update main.jsx

--- Front-SoftwareDT/src/main.jsx
+++ Front-SoftwareDT/src/main.jsx
@@ -29,7 +29,7 @@ ReactDOM.createRoot(rootElement).render(
             v7_relativeSplatPath: true 
           }}
         >
-          {/* 🔔 SISTEMA DE NOTIFICACIONES INDUSTRIAL */}
+
 
           <App />
         </BrowserRouter>
