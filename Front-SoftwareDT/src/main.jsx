7f586d9bdce510b280d2c087267effe4abfe9c92
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-07-07 23:54:34 -0500

    Update main.jsx

--- Front-SoftwareDT/src/main.jsx
+++ Front-SoftwareDT/src/main.jsx
@@ -1,16 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { UserProvider } from './context/UserContext'; 
-import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css"; 
-
-import App from './App'; 
-
-
-
-
-const rootElement = document.getElementById('root');
