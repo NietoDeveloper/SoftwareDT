00fae352fbb2e2b5a0e3d7b168434488b98b8b8a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-07-08 00:00:43 -0500

    Update App.jsx

--- Front-SoftwareDT/src/App.jsx
+++
@@ -1,5 +0,0 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
-import { Routes, Route, useLocation, useNavigate, Navigate } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import { useUser } from "./context/UserContext";
