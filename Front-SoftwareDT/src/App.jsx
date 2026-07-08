caac1702e6bc6862d8387a110db6c971520681f4
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-07-08 00:00:35 -0500

    Update App.jsx

--- Front-SoftwareDT/src/App.jsx
+++ Front-SoftwareDT/src/App.jsx
@@ -3,8 +3,3 @@ import React, { useEffect } from "react";
 import { Routes, Route, useLocation, useNavigate, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useUser } from "./context/UserContext";
-import { setupInterceptors } from "./API/api";
-
-// Components & Layout
-import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx"; 
