b56619f9b335065d675d91753b02e2975d98f0c6
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-07-08 23:51:17 -0500

    Update firebase.js

--- Front-SoftwareDT/src/firebase.js
+++ Front-SoftwareDT/src/firebase.js
@@ -1,7 +1,3 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-let app;
-if (getApps().length === 0) {
-
