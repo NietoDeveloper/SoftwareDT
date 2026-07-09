e6803bba00156122588d7936f2f5bfe556247309
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-07-08 23:46:46 -0500

    Update firebase.js

--- Front-SoftwareDT/src/firebase.js
+++ Front-SoftwareDT/src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = JSON.parse(typeof firebaseConfig !== 'undefined' ? firebaseConfig : '{}');
+const firebaseConfig = JSON.parse(typeof fonfig !== 'undefined' ? firebaseConfig : '{}');
 
 let app;
 if (getApps().length === 0) {
